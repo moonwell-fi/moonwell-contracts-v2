ec0771f042ff1b0c9bb35dca1573da7539b75f83
Elliot <elliotfriedman3@gmail.com>
2023-07-31 18:22:16 -0700

    hundred finance exploit tests
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@ forge test -vvv --match-contract UnitTest
 forge test --match-contract IntegrationTest --fork-url $ETH_RPC_URL -vvv
 forge test --match-contract ArbitrumTest --fork-url $ARB_RPC_URL -vvv
 forge test --match-contract LiveSystemTest --fork-url baseGoerli -vvv
+forge test --match-contract HundredFinanceExploitTest --fork-url baseGoerli -vvv
