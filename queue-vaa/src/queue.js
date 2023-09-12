09d7591cb6cf43cbda45668ad5139d1238be06d3
Luke Youngblood <luke@moonwell.fi>
2023-09-12 16:50:57 -0700

    Adds Discord notifications for VAA queue/execution

--- queue-vaa/src/queue.js
+++ queue-vaa/src/queue.js
@@ -4,15 +4,16 @@ const {
   DefenderRelayProvider
 } = require('defender-relay-client/lib/ethers');
 const { KeyValueStoreClient } = require('defender-kvstore-client');
+const axios = require('axios');
 
 const temporalGovernorABI = [
   "function queueProposal(bytes VAA)",
   "function executeProposal(bytes VAA)",
 ]
 
-// Use Moonbeam for Base
+// moonbeam is for Base Mainnet
 const network = 'moonbeam'
-// Use Moonbase Alpha for Base Goerli
+// moonbeam is for Base testnet
 // const network = 'moonbase'
 
 const tgAddress =
@@ -23,6 +24,87 @@ const tgAddress =
 const blockExplorer = 
   network === 'moonbase' ? 'https://goerli.basescan.org/tx/'
   : 'https://basescan.org/tx/'
+
+class MoonwellEvent {
+  async sendDiscordMessage(url, payload) {
+    console.log("Sending Discord message...");
+    if (!url) {
+      throw new Error("Discord webhook url is invalid!");
+    }
+    console.log("SENDING", JSON.stringify(payload, null, 2));
+    try {
+      const response = await axios.post(url, payload, {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      });
+      console.log(`Status code: ${response.status}`);
+      console.log(`Response data: ${JSON.stringify(response.data, null, 2)}`);
+    } catch (error) {
+      console.error(`Error message: ${error.message}`);
+    }
+    console.log("Sent Discord message!");
+  }
+
+  discordMessagePayload(color, text, txURL, networkName, sequence, timestamp, details) {
+    const baseFields = [
+      {
+        "name": "Network",
+        "value": networkName,
+        "inline": true
+      },
+      {
+        "name": "Sequence ID",
+        "value": sequence,
+        "inline": true
+      }
+    ];
+
+    if ((timestamp) && (timestamp > 0)) {
+      baseFields.push({
+        "name": "Will be executed at",
+        "value": `<t:${timestamp}>`,
+        "inline": true
+      });
+    }
+
+    if (details) {
+      baseFields.push({
+        "name": "Details",
+        "value": details,
+        "inline": false
+      });
+    }
+
+    return {
+      "content": "",
+      "embeds": [
+        {
+          "title": `${text}`,
+          "color": color,
+          "fields": baseFields,
+          "url": txURL
+        }
+      ]
+    };
+  }
+
+  discordFormatLink(text, url) {
+    return `[${text}](${url})`
+  }
+
+  numberWithCommas(num) {
+    if (!num.includes('.')) {
+      num = num + '.0'
+    }
+    const parts = num.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".")
+      .replace(/(\.\d+[1-9])0+$/, '$1')
+      .replace(/\.0+$/, '')
+  }
+}
 
 const fetchVAA = async (sequence, retries = 5, delay = 2000) => {
   const apiEndpoint = `https://vaa-fetch.moonwell.workers.dev/?network=${network}&sequence=${sequence}`;
@@ -73,7 +155,8 @@ async function storeVAA(event, sequence) {
 }
 
 // Entrypoint for the Autotask
-exports.handler = async function(event) {
+exports.handler = async function(event, context) {
+  const { notificationClient } = context;
   const payload = event.request.body;
   const matchReasons = payload.matchReasons;
   const sentinel = payload.sentinel;
@@ -94,6 +177,26 @@ exports.handler = async function(event) {
   const tx = await contract.queueProposal(vaa);
   console.log(`Called queueProposal in ${tx.hash}`);
   await storeVAA(event, sequence);
+  notificationClient.send({
+    channelAlias: 'Parameter Changes to Slack',
+    subject: `Inserted queued VAA ${sequence} on ${network}`,
+    message: `Inserted a queued key/value at future timestamp ${timestamp}, stored sequence value ${value} for ${network}`
+  });
+  const { GOVBOT_WEBHOOK } = credentials.secrets;
+  const moonwellEvent = new MoonwellEvent();
+  const friendlyNetworkName = 
+    networkName === 'moonbase' ? 'Base Goerli'
+    : 'Base'
+  const discordPayload = moonwellEvent.discordMessagePayload(
+    0x00ff00,
+    `Scheduled cross-chain execution of sequence ${sequence} on ${network}`,
+    blockExplorer + tx.hash,
+    friendlyNetworkName,
+    sequence,
+    timestamp,
+    `Upon successful completion of a governance proposal, it should be automatically executed on the ${friendlyNetworkName} network exactly 24 hours from now.`
+  );
+  await moonwellEvent.sendDiscordMessage(GOVBOT_WEBHOOK, discordPayload);
   return { tx: tx.hash };
 }
 
