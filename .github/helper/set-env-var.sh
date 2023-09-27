d988d047379863a437c8f9ded07891f94961acd8
Elliot <elliotfriedman3@gmail.com>
2023-09-27 12:31:03 +0200

    correct proposal artifact path
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- .github/helper/set-env-var.sh
+++ .github/helper/set-env-var.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Define the base directory
 BASE_DIR="artifacts/foundry"
 
-# Find the highest numbered mip-bXX directory
 LATEST_MIP_DIR=$(ls -1v ${BASE_DIR}/ | grep '^mip-b' | tail -n 1)
 
-# Output the command to set the PROPOSAL_ARTIFACT_PATH environment variable
-echo "PROPOSAL_ARTIFACT_PATH=${BASE_DIR}/${LATEST_MIP_DIR}/mipb${LATEST_MIP_DIR:6}.json" >> $GITHUB_ENV
+MIP_NUM=${LATEST_MIP_DIR:5:2}
+
+echo "PROPOSAL_ARTIFACT_PATH=${BASE_DIR}/${LATEST_MIP_DIR}/mipb${MIP_NUM}.json" >> $GITHUB_ENV
