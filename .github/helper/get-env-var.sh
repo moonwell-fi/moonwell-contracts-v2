cda78e9a5db24ce77352b63cbab0ba431fe52ddb
Elliot <elliotfriedman3@gmail.com>
2023-09-28 11:52:37 +0200

    add getter
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

---
+++ .github/helper/get-env-var.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+BASE_DIR="artifacts/foundry"
+
+LATEST_MIP_DIR=$(ls -1v ${BASE_DIR}/ | grep '^mip-b' | tail -n 1)
+
+MIP_NUM=${LATEST_MIP_DIR:5:2}
+
+echo "PROPOSAL_ARTIFACT_PATH=${BASE_DIR}/${LATEST_MIP_DIR}/mipb${MIP_NUM}.json"
