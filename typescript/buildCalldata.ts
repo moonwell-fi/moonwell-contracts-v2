12856f8bc3bbc1ce3947563271cf9a9f4abc06af
Elliot <elliotfriedman3@gmail.com>
2023-11-08 14:18:47 -0800

    correctly build and encode calldata for temporal governor
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- typescript/buildCalldata.ts
+++ typescript/buildCalldata.ts
@@ -1,7 +1,13 @@
 import * as fs from 'fs';
 import * as abi from 'ethereumjs-abi';
-import * as jsonData from './../proposals/MIP06.json';
 import BigNumber from "bignumber.js";
+import { ethers } from "ethers";
+
+if (process.env.PROPOSAL === undefined) {
+    throw new Error("No proposal file specified. Please specify a proposal file using the PROPOSAL environment variable.");
+}
+
+const jsonData = JSON.parse(fs.readFileSync(process.env.PROPOSAL, 'utf8'));
 
 /// @notice this is a script to generate calldata for temporal governor on BASE mainnet ONLY
 /// DO NOT use this script to generate calldata for any other network
@@ -11,8 +17,21 @@ import BigNumber from "bignumber.js";
 ///     add support for debugging output
 ///     add support for logging out description in debug mode
 
+///
+///     things tested so far for function arguments:
+///      - numbers
+///      - strings
+///      - addresses
+///      - booleans (true)
+///     TODO, need to test:
+///      - booleans (false)
+///      - arrays of numbers
+///      - arrays of strings
+///      - arrays of addresses
+///      - arrays of booleans
+
 // Load address data
-const addressData = JSON.parse(fs.readFileSync('./../utils/Addresses.json', 'utf8'));
+const addressData = JSON.parse(fs.readFileSync('./utils/Addresses.json', 'utf8'));
 const addressMap: { [key: string]: string } = {};
 
 /// max chainId is 2^53 - 1, so this will safely load addresses from BASE mainnet and all testnets
@@ -23,26 +42,15 @@ addressData.forEach((item: { name: string; addr: string, chainId: number }) => {
     }
 });
 
-// Function to replace variables in a string with their corresponding address
-// function replaceVariablesWithAddress(str: string): string {
-//     if str.match()
-//     return str.replace(/^[a-zA-Z]+(_[a-zA-Z]+)*$/g, (_, variable) => addressMap[variable] || `{${variable}}`);
-// }
-
 function replaceVariablesWithAddress(str: string): string {
     const regex = /^\{[a-zA-Z]+(_[a-zA-Z]+)*\}$/g
     if (regex.test(str)) {
-        console.log("str matched: ", str);
-
         const key = str.slice(1, -1);  // Remove the front and back braces
         return addressMap[key];
     }
 
-    console.log("str did not match: ", str);
-
     return str;
 }
-
 
 /// returns calldata of abi encoded method with arguments
 function encodeAbi(method: string, args: any[]): Buffer {
@@ -50,33 +58,25 @@ function encodeAbi(method: string, args: any[]): Buffer {
     const methodName = splitMethod[0];
     const types = splitMethod[1].slice(0, -1).split(',');
 
-    // console.log("types: ", types);
-
     const encodedMethod = abi.methodID(methodName, types);
     const encodedParams = abi.rawEncode(types, args);
-
-    // console.log("encodedMethod: ", encodedMethod.toString('hex'));
-    // console.log("encodedParams: ", encodedParams.toString('hex'));
-    // console.log("\n");
 
     return Buffer.concat([encodedMethod, encodedParams]);
 }
 
-
 /// return this abi encoded: (address, address[], uint256[], bytes[])
 function abiEncodeCalldata(calldata: TemporalGovCalldata): string {
-    /// TODO encode items one by one here to compare to solidity output
-    
-    const encodedData = abi.rawEncode(
+    const encodedData = ethers.utils.defaultAbiCoder.encode(
         ['address', 'address[]', 'uint256[]', 'bytes[]'],
         [
             calldata.temporalGovernorAddress,
             calldata.targets,
             calldata.values,
-            calldata.calldata.map(data => abi.rawEncode(['bytes'], [data]))
+            calldata.calldata
         ]
     );
-    return encodedData.toString('base64');
+
+    return encodedData;
 }
 
 interface TemporalGovCalldata {
@@ -91,11 +91,6 @@ function isNumerical(arg: string): boolean {
     return numericalPattern.test(arg);
 }
 
-function base64ToHex(base64String: string): string {
-    const rawBytes = Buffer.from(base64String, 'base64');
-    return rawBytes.toString('hex');
-}
-
 function buildTemporalGovCalldata(): string {
     let calldata: TemporalGovCalldata = { targets: [], calldata: [], values: [], temporalGovernorAddress: addressMap['TEMPORAL_GOVERNOR'] };
 
@@ -103,12 +98,10 @@ function buildTemporalGovCalldata(): string {
         const target = replaceVariablesWithAddress(command.target);
         const method = command.method;
 
-        console.log();
-
         /// arguments are either strings or numbers
         const args = command.arguments.map((arg: string) => {
-            console.log(arg);
-            console.log(typeof arg);
+            // console.log(arg);
+            // console.log(typeof arg);
 
             return isNumerical(arg) ?
                 (new BigNumber(arg)).toString() : /// if number, convert to BigNumber to prevent overflow errors and then cast to string,
@@ -116,8 +109,8 @@ function buildTemporalGovCalldata(): string {
                 replaceVariablesWithAddress(arg); /// if string, replace address variables with actual addresses, if no replacement, just use string
         }); /// intepret arguments as strings, and replace variables with addresses
 
-        console.log("target: ", target);
-        console.log("args: ", args);
+        // console.log("target: ", target);
+        // console.log("args: ", args);
 
         /// expected that values is 0
         if (target === undefined) {
@@ -137,12 +130,13 @@ function buildTemporalGovCalldata(): string {
         }
 
         calldata.targets.push(target);
-        calldata.calldata.push(encodeAbi(method, args)); /// error here
+        calldata.calldata.push(encodeAbi(method, args));
         calldata.values.push(0);
     });
 
     return abiEncodeCalldata(calldata);
 }
 
-console.log(buildTemporalGovCalldata()); /// error
-console.log(`proposal bytes: ${base64ToHex(buildTemporalGovCalldata())}`);
+process.stdout.write(buildTemporalGovCalldata());
+// console.log(buildTemporalGovCalldata());
+// console.log(`proposal bytes: ${base64ToHex(buildTemporalGovCalldata())}`);
