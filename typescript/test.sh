f1204a2885aaef21a46eb0ebe5cbd62f2ef5ceb6
Elliot <elliotfriedman3@gmail.com>
2023-11-08 16:07:07 -0800

    update environment variables to point to new JSON files
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- typescript/test.sh
+++ typescript/test.sh
@@ -1,22 +1,20 @@
-echo "MIP02"
+echo "------------- MIP02 -------------"
 echo ""
-PROPOSAL=./../proposals/MIP02.json npx ts-node buildCalldata.ts
-cd ..
+PROPOSAL=./proposals/MIPB02.json npx ts-node typescript/buildCalldata.ts
+echo "\n\n\n"
 forge script src/proposals/mips/mip-b02/mip-b02.sol:mipb02 --rpc-url base -vvvvv
-cd typescript
 
-echo "MIP05"
-echo ""
+echo "------------- MIP05 -------------"
+echo "\n\n\n"
 
-PROPOSAL=./../proposals/MIP05.json npx ts-node buildCalldata.ts
-cd ..
+PROPOSAL=./proposals/MIPB05.json npx ts-node typescript/buildCalldata.ts
+echo "\n\n\n"
 forge script src/proposals/mips/mip-b05/mip-b05.sol:mipb05 --rpc-url base -vvvvv
-cd typescript
 
-echo "MIP06"
-echo ""
+echo "------------- MIP06 -------------"
+echo "\n\n\n"
 
-PROPOSAL=./../proposals/MIP06.json npx ts-node buildCalldata.ts
-cd ..
+PROPOSAL=./proposals/MIPB06.json npx ts-node typescript/buildCalldata.ts
+echo "\n\n\n"
 forge script src/proposals/mips/mip-b06/mip-b06.sol:mipb06 --rpc-url base -vvvvv
 
