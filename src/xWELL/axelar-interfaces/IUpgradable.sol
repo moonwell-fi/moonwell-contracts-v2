ba99d65a69de156c8374716e72b8d11c569ef1b4
Elliot <elliotfriedman3@gmail.com>
2023-11-18 15:16:18 -0800

    axelar interfaces and library
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

---
+++ src/xWELL/axelar-interfaces/IUpgradable.sol
@@ -0,0 +1,41 @@
+/*
+  
+ ██████  ██████   ██████  ██   ██ ██████   ██████   ██████  ██   ██    ██████  ███████ ██    ██
+██      ██    ██ ██    ██ ██  ██  ██   ██ ██    ██ ██    ██ ██  ██     ██   ██ ██      ██    ██
+██      ██    ██ ██    ██ █████   ██████  ██    ██ ██    ██ █████      ██   ██ █████   ██    ██
+██      ██    ██ ██    ██ ██  ██  ██   ██ ██    ██ ██    ██ ██  ██     ██   ██ ██       ██  ██
+ ██████  ██████   ██████  ██   ██ ██████   ██████   ██████  ██   ██ ██ ██████  ███████   ████
+
+Find any smart contract, and build your project faster: https://www.cookbook.dev
+Twitter: https://twitter.com/cookbook_dev
+Discord: https://discord.gg/WzsfPcfHrk
+
+Find this contract on Cookbook: https://www.cookbook.dev/contracts/0x4fed5491693007f0cd49f4614ffc38ab6a04b619-AxelarBridge/?utm=code
+*/
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import { IOwnable } from './IOwnable.sol';
+import { IContractIdentifier } from './IContractIdentifier.sol';
+
+// General interface for upgradable contracts
+interface IUpgradable is IOwnable, IContractIdentifier {
+    error InvalidCodeHash();
+    error InvalidImplementation();
+    error SetupFailed();
+    error NotProxy();
+
+    event Upgraded(address indexed newImplementation);
+
+    function implementation() external view returns (address);
+
+    function upgrade(
+        address newImplementation,
+        bytes32 newImplementationCodeHash,
+        bytes calldata params
+    ) external;
+
+    function setup(bytes calldata data) external;
+}
