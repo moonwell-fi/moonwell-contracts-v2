da3a10686e5b3471fa624e675e159b938fccfa04
Elliot <elliotfriedman3@gmail.com>
2024-01-19 11:11:32 -0800

    deploy script working
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- src/Governance/MultichainGovernor/MultichainGovernorDeploy.sol
+++ src/Governance/MultichainGovernor/MultichainGovernorDeploy.sol
@@ -4,14 +4,17 @@ import {TransparentUpgradeableProxy, ITransparentUpgradeableProxy} from "@openze
 import {ProxyAdmin} from "@openzeppelin-contracts/contracts/proxy/transparent/ProxyAdmin.sol";
 
 import {MultichainGovernor} from "@protocol/Governance/MultichainGovernor/MultichainGovernor.sol";
+import {WormholeTrustedSender} from "@protocol/Governance/WormholeTrustedSender.sol";
 
 contract MultichainGovernorDeploy {
     function deployMultichainGovernor(
-        MultichainGovernor.InitializeData memory initData
+        MultichainGovernor.InitializeData memory initializeData,
+        WormholeTrustedSender.TrustedSender[] memory trustedSenders
     ) public returns (address proxyAdmin, address proxy, address governorImpl) {
         bytes memory initData = abi.encodeWithSignature(
-            "initialize((address,address,address,address,uint256,uint256,uint256,uint256,uint256,uint256,uint128,address,address,(uint16,address)[]))",
-            initData
+            "initialize((address,address,address,address,uint256,uint256,uint256,uint256,uint256,uint256,uint128,address,address,address),(uint16,address)[])",
+            initializeData,
+            trustedSenders
         );
 
         proxyAdmin = address(new ProxyAdmin());
