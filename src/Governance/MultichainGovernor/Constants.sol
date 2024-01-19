fe090612ba47a758e81a6b78099496916a727441
Elliot <elliotfriedman3@gmail.com>
2024-01-19 11:08:50 -0800

    sane max value parameters
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- src/Governance/MultichainGovernor/Constants.sol
+++ src/Governance/MultichainGovernor/Constants.sol
@@ -18,6 +18,14 @@ library Constants {
     /// @notice value for an abstain vote
     uint8 public constant VOTE_VALUE_ABSTAIN = 2;
 
-    /// @notice the number of average seconds per block
-    uint256 public constant MOONBEAM_BLOCK_TIME = 12;
+    /// @notice the minimum amount of time for cross chain vote collection.
+    /// This ensures that votes cast on other chains have the ability to
+    /// be registered even if Wormhole experiences downtime or delays.
+    uint256 public constant MIN_CROSS_CHAIN_VOTE_COLLECTION_PERIOD = 1 days;
+
+    /// @notice the minimum amount of voting power for the proposal threshold
+    uint256 public constant MIN_PROPOSAL_THRESHOLD = 1_000_000 * 1e18;
+
+    /// @notice the minimum voting period for a proposal, ensures proposals cannot be passed too quickly
+    uint256 public constant MIN_VOTING_PERIOD = 2 days;
 }
