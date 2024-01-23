9c1ead8a4b42ed63a038a42a0e4e2bfa4a3c8fa5
Ana Julia <anajuliabit@gmail.com>
2024-01-22 21:41:24 -0300

    test stake

--- test/unit/StakedWell.t.sol
+++ test/unit/StakedWell.t.sol
@@ -1,11 +1,56 @@
 pragma solidity 0.8.19;
 
 import "@forge-std/Test.sol";
-contract StakedWellUnitTest is Test {
-    function setUp() public {
-        address stakedWell = deployCode(
-            "StakedWell",
-            "stkWell/src/StakedWell.sol"
+
+import "@test/helper/BaseTest.t.sol";
+
+interface IStakedWell {
+    function initialize(
+        address _stakedToken,
+        address _rewardToken,
+        uint256 _cooldownPeriod,
+        uint256 _unstakePeriod,
+        address _rewardsVault,
+        address _emissionManager,
+        uint128 _distributionDuration,
+        address _governance
+    ) external;
+}
+
+contract StakedWellUnitTest is BaseTest {
+    IStakedWell stakedWell;
+    uint256 cooldown;
+    uint256 unstakePeriod;
+    uint256 amount;
+    function setUp() public override {
+        super.setUp();
+        address stakedWellAddress = deployCode(
+            "StakedWell.sol:StakedWell",
+            "stkWell/artifacts/StakedWell.sol/StakedWell.json"
         );
+        stakedWell = IStakedWell(stakedWellAddress);
+
+        cooldown = 1 days;
+        unstakePeriod = 3 days;
+
+        stakedWell.initialize(
+            address(xwellProxy),
+            address(xwellProxy),
+            cooldown,
+            unstakePeriod,
+            address(this),
+            address(this),
+            1,
+            address(this)
+        );
+
+        amount = 10_000 ** 10 * 18;
+        vm.prank(address(xerc20Lockbox));
+        xwellProxy.mint(address(this), amount);
+    }
+
+    function testStake() public {
+        uint256 balance = xwellProxy.balanceOf(address(this));
+        console.log(balance);
     }
 }
