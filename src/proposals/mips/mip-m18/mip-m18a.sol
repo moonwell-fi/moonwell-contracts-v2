541084543eb4a5679430e9256fd82a43c09dfd57
Elliot <elliotfriedman3@gmail.com>
2024-01-29 23:28:47 -0800

    proposal mip-18-a post deployment validations
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- src/proposals/mips/mip-m18/mip-m18a.sol
+++ src/proposals/mips/mip-m18/mip-m18a.sol
@@ -9,7 +9,16 @@ import {MultichainGovernorDeploy} from "@protocol/Governance/MultichainGovernor/
 
 /// Proposal to run on Moonbeam to create the Multichain Governor contract
 contract mipm18a is HybridProposal, MultichainGovernorDeploy {
+    /// @notice deployment name
     string public constant name = "MIP-M18A";
+
+    /// @notice slot for the Proxy Admin
+    bytes32 _ADMIN_SLOT =
+        0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103;
+
+    /// @notice slot for the implementation address
+    bytes32 _IMPLEMENTATION_SLOT =
+        0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
     function deploy(Addresses addresses, address) public override {
         address proxyAdmin = addresses.getAddress("MOONBEAM_PROXY_ADMIN");
@@ -35,7 +44,38 @@ contract mipm18a is HybridProposal, MultichainGovernorDeploy {
     }
 
     function validate(Addresses addresses, address) public override {
-        /// TODO validate that pending owners have been set where appropriate
-        /// TODO validate that new admin/owner has been set where appropriate
+        {
+            bytes32 data = vm.load(
+                addresses.getAddress("MULTICHAIN_GOVERNOR_PROXY"),
+                _ADMIN_SLOT
+            );
+
+            assertEq(
+                bytes32(
+                    uint256(uint160(addresses.getAddress("MRD_PROXY_ADMIN")))
+                ),
+                data,
+                "mrd proxy admin not set correctly"
+            );
+        }
+
+        {
+            bytes32 data = vm.load(
+                addresses.getAddress("MULTICHAIN_GOVERNOR_PROXY"),
+                _IMPLEMENTATION_SLOT
+            );
+
+            assertEq(
+                bytes32(
+                    uint256(
+                        uint160(
+                            addresses.getAddress("MULTICHAIN_GOVERNOR_IMPL")
+                        )
+                    )
+                ),
+                data,
+                "mrd implementation not set correctly"
+            );
+        }
     }
 }
