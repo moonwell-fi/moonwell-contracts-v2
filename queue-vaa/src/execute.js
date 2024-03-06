8a647f3c7407f82721856ad67a52e7492df2f697
anajuliabit <anajuliabit@gmail.com>
2024-03-06 16:32:16 -0300

    Make queueVAA compatible with sepolia

--- queue-vaa/src/execute.js
+++ queue-vaa/src/execute.js
@@ -17,13 +17,15 @@ const network = 'moonbeam'
 // const network = 'moonbase'
 
 const tgAddress =
-  network === 'moonbase' ? '0xBaA4916ACD2d3Db77278A377f1b49A6E1127d6e6' // TemporalGovernor on Base Goerli
-  : '0x8b621804a7637b781e2BbD58e256a591F2dF7d51' // TemporalGovernor on Base
+    network === 'moonbase'
+        ? '0xc01EA381A64F8BE3bDBb01A7c34D809f80783662' // TemporalGovernor on Base Sepolia
+        : '0x8b621804a7637b781e2BbD58e256a591F2dF7d51'; // TemporalGovernor on Base
 
 // Block explorer URL
-const blockExplorer = 
-  network === 'moonbase' ? 'https://goerli.basescan.org/tx/'
-  : 'https://basescan.org/tx/'
+const blockExplorer =
+    network === 'moonbase'
+        ? 'https://sepolia.basescan.org/tx/'
+        : 'https://basescan.org/tx/';
 
 class MoonwellEvent {
   async sendDiscordMessage(url, payload) {
@@ -47,31 +49,53 @@ class MoonwellEvent {
     console.log("Sent Discord message!");
   }
 
-  discordMessagePayload(color, resultText, txURL, networkName, sequence, timestamp) {
-    const friendlyNetworkName =
-      networkName === 'moonbase' ? 'Base Goerli'
-        : 'Base'
-    const mipNumber = sequence - 1;
-    let mipString = '';
-    if (mipNumber < 10) {
-      mipString = `MIP-B0${mipNumber}`;
-    } else {
-      mipString = `MIP-B${mipNumber}`;
-    }
-    const text = `${resultText.slice(0, 1).toUpperCase()}${resultText.slice(1)} ${mipString} on ${friendlyNetworkName}`;
-    const details = `Governance proposal ${mipString} ${resultText} on the ${friendlyNetworkName} network.`;
-    const baseFields = [
-      {
-        "name": "Network",
-        "value": friendlyNetworkName,
-        "inline": true
-      },
-      {
-        "name": "Proposal",
-        "value": mipString,
-        "inline": true
-      }
-    ];
+    discordMessagePayload(
+        color,
+        resultText,
+        txURL,
+        networkName,
+        sequence,
+        timestamp,
+    ) {
+        const friendlyNetworkName =
+            networkName === 'moonbase' ? 'Base Sepolia' : 'Base';
+        const mipNumber = sequence - 1;
+        let mipString = '';
+        if (mipNumber < 10) {
+            mipString = `MIP-B0${mipNumber}`;
+        } else {
+            mipString = `MIP-B${mipNumber}`;
+        }
+        const text = `${resultText.slice(0, 1).toUpperCase()}${resultText.slice(1)} ${mipString} on ${friendlyNetworkName}`;
+        const details = `Governance proposal ${mipString} ${resultText} on the ${friendlyNetworkName} network.`;
+        const baseFields = [
+            {
+                name: 'Network',
+                value: friendlyNetworkName,
+                inline: true,
+            },
+            {
+                name: 'Proposal',
+                value: mipString,
+                inline: true,
+            },
+        ];
+
+        if (timestamp && timestamp > 0) {
+            baseFields.push({
+                name: 'Executed at',
+                value: `<t:${timestamp}>`,
+                inline: true,
+            });
+        }
+
+        if (details) {
+            baseFields.push({
+                name: 'Details',
+                value: details,
+                inline: false,
+            });
+        }
 
     if ((timestamp) && (timestamp > 0)) {
       baseFields.push({
--- queue-vaa/src/queue.js
+++ queue-vaa/src/queue.js
@@ -17,13 +17,15 @@ const network = 'moonbeam'
 // const network = 'moonbase'
 
 const tgAddress =
-  network === 'moonbase' ? '0xBaA4916ACD2d3Db77278A377f1b49A6E1127d6e6' // TemporalGovernor on Base Goerli
-  : '0x8b621804a7637b781e2BbD58e256a591F2dF7d51' // TemporalGovernor on Base
+    network === 'moonbase'
+        ? '0xc01EA381A64F8BE3bDBb01A7c34D809f80783662' // TemporalGovernor on Base Sepolia
+        : '0x8b621804a7637b781e2BbD58e256a591F2dF7d51'; // TemporalGovernor on Base
 
 // Block explorer URL
-const blockExplorer = 
-  network === 'moonbase' ? 'https://goerli.basescan.org/tx/'
-  : 'https://basescan.org/tx/'
+const blockExplorer =
+    network === 'moonbase'
+        ? 'https://sepolia.basescan.org/tx/'
+        : 'https://basescan.org/tx/';
 
 class MoonwellEvent {
   async sendDiscordMessage(url, payload) {
