7fd8117c299e7461e3a95f8afd56a2ceb7999042
anajuliabit <anajuliabit@gmail.com>
2024-03-14 14:42:13 -0300

    execute 18 b

--- src/proposals/proposalTypes/HybridProposal.sol
+++ src/proposals/proposalTypes/HybridProposal.sol
@@ -223,13 +223,19 @@ abstract contract HybridProposal is
     function getTargetsPayloadsValues(
         Addresses addresses
     ) public view returns (address[] memory, uint256[] memory, bytes[] memory) {
+        address temporalGovernor;
+        if (addresses.isAddressSet("TEMPORAL_GOVERNOR")) {
+            temporalGovernor = addresses.getAddress("TEMPORAL_GOVERNOR");
+        } else {
+            temporalGovernor = addresses.getAddress(
+                "TEMPORAL_GOVERNOR",
+                sendingChainIdToReceivingChainId[block.chainid]
+            );
+        }
         return
             getTargetsPayloadsValues(
                 addresses.getAddress("WORMHOLE_CORE"),
-                addresses.getAddress(
-                    "TEMPORAL_GOVERNOR",
-                    sendingChainIdToReceivingChainId[block.chainid]
-                )
+                temporalGovernor
             );
     }
 
