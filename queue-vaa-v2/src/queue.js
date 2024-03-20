3604aeec8c98f74c74c84095d2b25c3a0bf59ef6
Ana Julia <anajuliabit@gmail.com>
2024-03-20 16:16:42 -0300

    queue working

--- queue-vaa-v2/src/queue.js
+++ queue-vaa-v2/src/queue.js
@@ -1,78 +1,120 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { ethers } = require("ethers");
-const { Defender } = require('@openzeppelin/defender-sdk');
+const {ethers} = require('ethers');
+const {Defender} = require('@openzeppelin/defender-sdk');
+const {KeyValueStoreClient} = require('defender-kvstore-client');
 const axios = require('axios');
 
 // moonbeam is for Base Mainnet
 //const network = 'moonbeam'
 // moonbeam is for Base testnet
- const network = 'moonbase'
+const network = 'moonbase';
 
 const temporalGovernorABI = [
-  "function queueProposal(bytes VAA)",
-  "function executeProposal(bytes VAA)",
-]
+    'function queueProposal(bytes VAA)',
+    'function executeProposal(bytes VAA)',
+];
 
 const tgAddress =
     network === 'moonbase'
         ? '0x5713dd9a2b2ce515cf9232f48b457aebfb04b292' // TemporalGovernor on Base Sepolia
         : '0x8b621804a7637b781e2BbD58e256a591F2dF7d51'; // TemporalGovernor on Base
 
-
 const fetchVAA = async (sequence, retries = 5, delay = 2000) => {
-  const apiEndpoint = `https://vaa-fetch.moonwell.workers.dev/?network=${network}&sequence=${sequence}`;
-  for (let i = 0; i <= retries; i++) {
-    try {
-      const response = await fetch(apiEndpoint, {
-        method: 'GET',
-        headers: {
-          'accept': 'application/json'
+    const apiEndpoint = `https://vaa-fetch.moonwell.workers.dev/?network=${network}&sequence=${sequence}`;
+    for (let i = 0; i <= retries; i++) {
+        try {
+            const response = await fetch(apiEndpoint, {
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const jsonResponse = await response.json();
+            return jsonResponse.vaa;
+        } catch (error) {
+            if (i === retries) {
+                throw error; // if it's the last retry, throw the error
+            }
+
+            // Wait for an exponentially increasing amount of time
+            await new Promise((res) => setTimeout(res, delay * 2 ** i));
         }
-      });
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-
-      const jsonResponse = await response.json();
-      return jsonResponse.vaa;
-    } catch (error) {
-      if (i === retries) {
-        throw error; // if it's the last retry, throw the error
-      }
-
-      // Wait for an exponentially increasing amount of time
-      await new Promise(res => setTimeout(res, delay * (2 ** i)));
     }
-  }
-  throw new Error("Failed to fetch VAA after multiple retries");
-}
+    throw new Error('Failed to fetch VAA after multiple retries');
+};
 
+async function storeVAA(event, sequence, timestamp) {
+    console.log('Storing sequence in KV store...');
+    const kvStore = new KeyValueStoreClient(event);
+    let value = await kvStore.get(network);
+    console.log(`Initial KV store value for ${network}: ${value}`);
+    if (value !== null) {
+        value += `,${sequence}`;
+    } else {
+        value = sequence;
+    }
+    await kvStore.put(network, value);
+    console.log(`Updated KV store value for ${network}: ${value}`);
+    await kvStore.put(`${network}-${sequence}`, timestamp.toString());
+    console.log(`Stored ${network}-${sequence} with expiry ${timestamp}`);
+}
 
 // Entrypoint for the action
 exports.handler = async function (event) {
-  if (!event.request || !event.request.body) throw new Error(`Missing payload`);
-  const { matchReasons, request, signature } = event.request.body;
+    console.log('Received event:', JSON.stringify(event.request.body, null, 2));
 
-  const sequence = payload.matchReasons[0].params.sequence;
-  const vaa = await fetchVAA(sequence);
+    const sequence = event.request.body.matchReasons[0].params.sequence;
+    const vaa = await fetchVAA(sequence);
 
-  const client = new Defender(event);
+    const client = new Defender(event);
 
-  const provider = client.relaySigner.getProvider();
-  const signer = client.relaySigner.getSigner(provider, { speed: 'fast' });
+    const provider = client.relaySigner.getProvider();
+    const signer = client.relaySigner.getSigner(provider, {speed: 'fast'});
 
-  const governor= new ethers.Contract(tgAddress, temporalGovernorABI, signer);
+    const contract = new ethers.Contract(
+        tgAddress,
+        temporalGovernorABI,
+        signer,
+    );
 
-  const tx = await contract.queueProposal(vaa);
-  console.log(`Called queueProposal in ${tx.hash}`);
-
-}
+    const tx = await contract.queueProposal(vaa);
+    console.log(`Called queueProposal in ${tx.hash}`);
+    let timestamp = Math.floor(Date.now() / 1000);
+    // On testnet, there is no 24 hour timelock
+    //timestamp += 60 * 60 * 24 + 60; // 24 hours + 1 minute for buffer
+    await storeVAA(event, sequence, timestamp);
+    notificationClient.send({
+        channelAlias: 'Parameter Changes to Slack',
+        subject: `Inserted queued VAA ${sequence} on ${network}`,
+        message: `Inserted a queued key/value at future timestamp ${timestamp} for ${network}`,
+    });
+    const {GOVBOT_WEBHOOK} = event.secrets;
+    const moonwellEvent = new MoonwellEvent();
+    const friendlyNetworkName = network === 'moonbase' ? 'Base Goerli' : 'Base';
+    const discordPayload = moonwellEvent.discordMessagePayload(
+        0x00ff00,
+        blockExplorer + tx.hash,
+        friendlyNetworkName,
+        sequence,
+        timestamp,
+    );
+    await moonwellEvent.sendDiscordMessage(GOVBOT_WEBHOOK, discordPayload);
+    return {tx: tx.hash};
+};
 
 // To run locally (this code will not be executed in Autotasks)
 if (require.main === module) {
-  const { API_KEY: apiKey, API_SECRET: apiSecret } = process.env;
-  exports.handler({ ...mockEvent, apiKey, apiSecret })
-    .then(() => process.exit(0))
-    .catch(error => { console.error(error); process.exit(1); });
+    const {API_KEY: apiKey, API_SECRET: apiSecret} = process.env;
+    exports
+        .handler({...mockEvent, apiKey, apiSecret})
+        .then(() => process.exit(0))
+        .catch((error) => {
+            console.error(error);
+            process.exit(1);
+        });
 }
