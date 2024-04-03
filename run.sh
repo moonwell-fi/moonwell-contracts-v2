e940805be903f792fd1b289cb5f083a6bf22925e
Elliot <elliotfriedman3@gmail.com>
2024-04-03 12:42:05 -0700

    remove integration test without match
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 forge test --match-contract UnitTest -vvv 
-forge test --match-contract IntegrationTest --fork-url ethereum -vvv
 forge test --match-contract ArbitrumTest --fork-url arbitrum -vvv
 forge test --match-contract MoonbeamTest --fork-url moonbeam -vvv
 forge test --match-contract LiveSystemTest --fork-url baseSepolia -vvv
