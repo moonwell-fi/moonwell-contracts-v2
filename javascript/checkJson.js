716921e116d42278d17021290ccb0298cdcb3b6e
Elliot <elliotfriedman3@gmail.com>
2024-04-08 17:48:40 -0700

    lint, change style

--- javascript/checkJson.js
+++ javascript/checkJson.js
@@ -1,14 +1,14 @@
 const jsonData = require('./utils/Addresses.json');
 
 for (const x of jsonData) {
-    if (typeof x.addr !== "string") {
+    if (typeof x.addr !== 'string') {
         console.log(`Error: address is not a string ${x.addr}`);
     }
-    if (typeof x.name !== "string") {
+    if (typeof x.name !== 'string') {
         console.log(`Error: name is not a string ${x.name}`);
     }
     if (isNaN(x.chainId)) {
         console.log(`Error: chainId is not a number ${x.chainId}`);
     }
-    console.log("checking element")
+    console.log('checking element');
 }
