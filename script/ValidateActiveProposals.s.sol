28d09752a0861c5a54f679bbca8305c1c5a7c72a
Ana Julia <anajuliabit@gmail.com>
2024-04-19 18:47:48 -0300

    execute proposal

--- script/ValidateActiveProposals.s.sol
+++ script/ValidateActiveProposals.s.sol
@@ -1,21 +1,33 @@
 pragma solidity 0.8.19;
 
+import "@forge-std/Test.sol";
 import {console} from "@forge-std/console.sol";
+
 import {IMultichainGovernor, MultichainGovernor} from "@protocol/governance/multichain/MultichainGovernor.sol";
+import {xWELL} from "@protocol/xWELL/xWELL.sol";
+import {ITemporalGovernor} from "@protocol/governance/TemporalGovernor.sol";
 import {Addresses} from "@proposals/Addresses.sol";
 import {Script} from "@forge-std/Script.sol";
 import {ChainIds} from "@test/utils/ChainIds.sol";
-import "@forge-std/Test.sol";
 
 contract ValidateActiveProposals is Script, Test, ChainIds {
     /// @notice addresses contract
     Addresses addresses;
+
+    /// @notice fork ID for base
+    uint256 public baseForkId =
+        vm.createFork(vm.envOr("BASE_RPC_URL", string("base")));
+
+    /// @notice fork ID for moonbeam
+    uint256 public moonbeamForkId =
+        vm.createFork(vm.envOr("MOONBEAM_RPC_URL", string("moonbeam")));
 
     constructor() {
         addresses = new Addresses();
     }
 
     function run() public {
+        vm.selectFork(moonbeamForkId);
         MultichainGovernor governor = MultichainGovernor(
             addresses.getAddress("MULTICHAIN_GOVERNOR_PROXY")
         );
@@ -33,28 +45,32 @@ contract ValidateActiveProposals is Script, Test, ChainIds {
                     "Proposal target not a contract"
                 );
 
-                // Simulate proposals execution
-                address well = addresses.getAddress("WELL");
+                {
+                    // Simulate proposals execution
+                    (
+                        ,
+                        uint256 voteSnapshotTimestamp,
+                        uint256 votingStartTime,
+                        ,
+                        uint256 crossChainVoteCollectionEndTimestamp,
+                        ,
+                        ,
+                        ,
 
-                // cast votes
-                deal(well, address(this), governor.quorum());
-                governor.castVote(proposalId, 0);
+                    ) = governor.proposalInformation(proposalId);
 
-                (
-                    ,
-                    ,
-                    ,
-                    ,
-                    uint256 crossChainVoteCollectionEndTimestamp,
-                    ,
-                    ,
-                    ,
+                    address well = addresses.getAddress("xWELL_PROXY");
+                    vm.warp(voteSnapshotTimestamp - 1);
+                    deal(well, address(this), governor.quorum());
+                    xWELL(well).delegate(address(this));
 
-                ) = governor.proposalInformation(proposalId);
+                    vm.warp(votingStartTime);
+                    governor.castVote(proposalId, 0);
 
-                vm.warp(crossChainVoteCollectionEndTimestamp + 1);
+                    vm.warp(crossChainVoteCollectionEndTimestamp + 1);
 
-                governor.execute(proposalId);
+                    governor.execute(proposalId);
+                }
             }
 
             // Check if there is any action on Base
@@ -74,65 +90,81 @@ contract ValidateActiveProposals is Script, Test, ChainIds {
                     ),
                     (uint32, bytes, uint8)
                 );
-
-                // decode payload
-                (
-                    address temporalGovernorAddress,
-                    address[] memory targets,
-                    uint256[] memory values,
-                    bytes[] memory payloads
-                ) = abi.decode(
-                        payload,
-                        (address, address[], uint256[], bytes[])
-                    );
-
                 address expectedTemporalGov = block.chainid == moonBeamChainId
                     ? addresses.getAddress("TEMPORAL_GOVERNOR", baseChainId)
                     : addresses.getAddress(
                         "TEMPORAL_GOVERNOR",
                         baseSepoliaChainId
                     );
-                require(
-                    temporalGovernorAddress == expectedTemporalGov,
-                    "Temporal Governor address mismatch"
+
+                {
+                    // decode payload
+                    (
+                        address temporalGovernorAddress,
+                        address[] memory targets,
+                        uint256[] memory values,
+                        bytes[] memory payloads
+                    ) = abi.decode(
+                            payload,
+                            (address, address[], uint256[], bytes[])
+                        );
+
+                    require(
+                        temporalGovernorAddress == expectedTemporalGov,
+                        "Temporal Governor address mismatch"
+                    );
+                }
+
+                vm.selectFork(baseForkId);
+
+                bytes memory vaa = generateVAA(
+                    uint32(block.timestamp),
+                    uint16(chainIdToWormHoleId[baseChainId]),
+                    addressToBytes(address(governor)),
+                    payload
                 );
+
+                ITemporalGovernor temporalGovernor = ITemporalGovernor(
+                    expectedTemporalGov
+                );
+
+                temporalGovernor.queueProposal(vaa);
+
+                vm.warp(block.timestamp + temporalGovernor.proposalDelay());
+
+                temporalGovernor.executeProposal(vaa);
             }
         }
     }
 
-    //    function executeOnBase() {
-    //        bytes memory payload = abi.encode(
-    //            temporalGovernorAddress,
-    //            targets,
-    //            values,
-    //            payloads
-    //        );
-    //
-    //        bytes32 governor = addressToBytes(
-    //            addresses.getAddress(
-    //                "MULTICHAIN_GOVERNOR_PROXY",
-    //                sendingChainIdToReceivingChainId[block.chainid]
-    //            )
-    //        );
-    //
-    //        bytes memory vaa = generateVAA(
-    //            uint32(block.timestamp),
-    //            uint16(chainIdToWormHoleId[baseChainId]),
-    //            governor,
-    //            payload
-    //        );
-    //
-    //        ITemporalGovernor temporalGovernor = ITemporalGovernor(
-    //            temporalGovernorAddress
-    //        );
-    //
-    //        temporalGovernor.queueProposal(vaa);
-    //
-    //        vm.warp(block.timestamp + temporalGovernor.proposalDelay());
-    //
-    //        temporalGovernor.executeProposal(vaa);
-    //
-    //    }
+    /// @dev utility function to generate a Wormhole VAA payload excluding the guardians signature
+    function generateVAA(
+        uint32 timestamp,
+        uint16 emitterChainId,
+        bytes32 emitterAddress,
+        bytes memory payload
+    ) private pure returns (bytes memory encodedVM) {
+        uint64 sequence = 200;
+        uint8 version = 1;
+        uint256 nonce = 1;
+        uint256 consistencyLevel = 200;
+
+        encodedVM = abi.encodePacked(
+            version,
+            timestamp,
+            nonce,
+            emitterChainId,
+            emitterAddress,
+            sequence,
+            consistencyLevel,
+            payload
+        );
+    }
+
+    /// @dev utility function to convert an address to bytes32
+    function addressToBytes(address addr) public pure returns (bytes32) {
+        return bytes32(bytes20(addr)) >> 96;
+    }
 
     // Utility function to slice bytes array
     function slice(
