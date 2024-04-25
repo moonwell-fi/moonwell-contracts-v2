29a6d80a4d746d40f98d8d81698b781d053a48c8
Ana Julia <anajuliabit@gmail.com>
2024-04-25 20:44:48 -0300

    filter mipb00

--- branch-proposals.sh
+++ branch-proposals.sh
@@ -9,5 +9,9 @@ pr_number=$(echo "$pr_details" | jq -r '.[0].number')
 
 # Step 3: Print changes if a PR number is found
 if [ ! -z "$pr_number" ]; then
-  gh pr diff $pr_number --name-only | grep 'src/proposals/mips/.*\.sol$'
+  # Get the list of modified .sol files in the 'src/proposals/mips/' directory
+  modified_files=$(gh pr diff $pr_number --name-only | grep 'src/proposals/mips/.*\.sol$')
+
+  # Filter out the specific file
+  echo "$modified_files" | grep -v 'src/proposals/mips/mip-b00/mip-b00.sol'
 fi
