04526c198f5f73a38f96f8d52873d1dbb47d610f
Elliot <elliotfriedman3@gmail.com>
2024-05-13 20:15:50 -0700

    moprho interfaces
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

---
+++ src/interfaces/IMorphoChainlinkOracleV2.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity >=0.5.0;
+
+import {IOracle} from "./IOracle.sol";
+import {AggregatorV3Interface} from "@protocol/oracles/AggregatorV3Interface.sol";
+
+/// @title IMorphoChainlinkOracleV2
+/// @author Morpho Labs
+/// @custom:contact security@morpho.org
+/// @notice Interface of MorphoChainlinkOracleV2.
+interface IMorphoChainlinkOracleV2 is IOracle {
+    /// @notice Returns the address of the base ERC4626 vault.
+    function BASE_VAULT() external view returns (address);
+
+    /// @notice Returns the base vault conversion sample.
+    function BASE_VAULT_CONVERSION_SAMPLE() external view returns (uint256);
+
+    /// @notice Returns the address of the quote ERC4626 vault.
+    function QUOTE_VAULT() external view returns (address);
+
+    /// @notice Returns the quote vault conversion sample.
+    function QUOTE_VAULT_CONVERSION_SAMPLE() external view returns (uint256);
+
+    /// @notice Returns the address of the first base feed.
+    function BASE_FEED_1() external view returns (AggregatorV3Interface);
+
+    /// @notice Returns the address of the second base feed.
+    function BASE_FEED_2() external view returns (AggregatorV3Interface);
+
+    /// @notice Returns the address of the first quote feed.
+    function QUOTE_FEED_1() external view returns (AggregatorV3Interface);
+
+    /// @notice Returns the address of the second quote feed.
+    function QUOTE_FEED_2() external view returns (AggregatorV3Interface);
+
+    /// @notice Returns the price scale factor, calculated at contract creation.
+    function SCALE_FACTOR() external view returns (uint256);
+}
