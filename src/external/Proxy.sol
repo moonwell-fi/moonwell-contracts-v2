1b8e0cd2c2a4ee7ae64bfa025284bb13c4950e47
Elliot <elliotfriedman3@gmail.com>
2024-05-15 17:21:56 -0700

    natspec
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- src/external/Proxy.sol
+++ src/external/Proxy.sol
@@ -7,14 +7,14 @@ import {SafeERC20} from "@openzeppelin-contracts/contracts/token/ERC20/utils/Saf
 contract Proxy is TransparentUpgradeableProxy {
     using SafeERC20 for IERC20;
 
-    /// logic contract on testnet: 0x10e5196375e6461703662DE1c11F0C28884cBf3b
-
-    // address owner,
-    // address morpho,
-    // uint256 initialTimelock,
-    // address _asset,
-    // string memory _name,
-    // string memory _symbol
+    /// @param _morpho The address of the Morpho contract
+    /// @param _logic The address of the logic contract
+    /// @param _admin The address of the admin to the proxy contract
+    /// @param _owner The address of the owner of the proxy contract, will control the MetaMorhpo Vault
+    /// @param _timelock The timelock for the proxy contract
+    /// @param _underlyingAsset The address of the underlying asset
+    /// @param _name The name of the token
+    /// @param _symbol The symbol of the token
     constructor(
         address _morpho,
         address _logic,
@@ -25,10 +25,16 @@ contract Proxy is TransparentUpgradeableProxy {
         string memory _name,
         string memory _symbol
     ) TransparentUpgradeableProxy(_logic, _admin, "") {
+        /// TODO test approval
         IERC20(_underlyingAsset).safeApprove(_morpho, type(uint256).max);
 
         require(bytes(_name).length <= 30, "name too long");
         require(bytes(_symbol).length <= 30, "symbol too long");
+
+        require(
+            _timelock >= 1 days && _timelock <= 2 weeks,
+            "timelock too short"
+        );
 
         /// variable  |  slot
         /// -----------------
@@ -43,11 +49,13 @@ contract Proxy is TransparentUpgradeableProxy {
         uint256 name;
         uint256 symbol;
 
+        /// set the name and symbol to the start of the string
         assembly {
             name := mload(add(_name, 32))
             symbol := mload(add(_symbol, 32))
         }
 
+        /// set the least significant byte to the length of the string * 2
         name |= uint8(bytes(_name).length * 2);
         symbol |= uint8(bytes(_symbol).length * 2);
 
