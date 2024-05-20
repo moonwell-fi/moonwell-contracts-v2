2c2a24ddc81654c3ac2e4642b997baf5dd1bbd98
Elliot <elliotfriedman3@gmail.com>
2024-05-19 23:48:01 -0700

    consolidate scripts into a single file

---
+++ script/Deploy4626RouterFactories.s.sol
@@ -0,0 +1,22 @@
+pragma solidity 0.8.19;
+
+import {Script} from "@forge-std/Script.sol";
+
+import {Addresses} from "@proposals/Addresses.sol";
+import {deployFactory, deploy4626Router, deployFactoryEth} from "@protocol/4626/4626FactoryDeploy.sol";
+
+/// forge script script/Deploy4626RouterFactories.s.sol:Deploy4626RouterFactories --fork-url base -vvv
+contract Deploy4626RouterFactories is Script {
+    /// @notice addresses contract
+    Addresses addresses;
+
+    constructor() {
+        addresses = new Addresses();
+    }
+
+    function run() public {
+        deployFactory(addresses);
+        deployFactoryEth(addresses);
+        deploy4626Router(addresses);
+    }
+}
