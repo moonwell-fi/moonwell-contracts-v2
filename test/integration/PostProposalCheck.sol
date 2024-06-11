14c0a7d99b2fba52acd6a336e6b40694fc4b4d00
Ana Julia <anajuliabit@gmail.com>
2024-06-10 21:14:00 -0300

    refactor PostProposalCheck

--- test/integration/PostProposalCheck.sol
+++ test/integration/PostProposalCheck.sol
@@ -5,68 +5,34 @@ import {Test} from "@forge-std/Test.sol";
 
 import {String} from "@utils/String.sol";
 import {Addresses} from "@proposals/Addresses.sol";
-import {CreateCode} from "@proposals/utils/CreateCode.sol";
-import {TestProposals} from "@proposals/TestProposals.sol";
 import {MockERC20Params} from "@test/mock/MockERC20Params.sol";
+import {Proposal} from "@proposals/proposalTypes/Proposal.sol";
 
-contract PostProposalCheck is CreateCode {
+contract PostProposalCheck is Test {
     using String for string;
 
-    Addresses addresses;
-    TestProposals proposals;
+    Addresses public addresses;
 
     function setUp() public virtual {
-        string memory path = getPath();
-        // Run all pending proposals before doing e2e tests
-        address[] memory mips = new address[](1);
+        addresses = new Addresses();
+        vm.makePersistent(address(addresses));
+
+        // get the latest base proposal
+        string[] memory inputs = new string[](1);
+        inputs[0] = "./get-latest-moonbeam-proposal.sh";
+
+        string memory output = string(vm.ffi(inputs));
+
+        Proposal latestProposal = Proposal(deployCode(output));
+        latestProposal.build(addresses);
 
         if (
-            keccak256(bytes(path)) == keccak256('""') || bytes(path).length == 0
+            latestProposal.checkOnChainCalldata(
+                addresses.getAddress("MULTICHAIN_GOVERNOR_PROXY")
+            )
         ) {
-            /// empty string on both mac and unix, no proposals to run
-            mips = new address[](0);
-
-            proposals = new TestProposals(mips);
-        } else if (path.hasChar(",")) {
-            string[] memory mipPaths = path.split(",");
-            if (mipPaths.length < 2) {
-                revert(
-                    "Invalid path(s) provided. If you want to deploy a single mip, do not use a comma."
-                );
-            }
-            mips = new address[](mipPaths.length); /// expand mips size if multiple mips
-
-            /// guzzle all of the memory, quadratic cost, but we don't care
-            for (uint256 i = 0; i < mipPaths.length; i++) {
-                /// deploy each mip and add it to the array
-                bytes memory code = getCode(mipPaths[i]);
-
-                mips[i] = deployCode(code);
-                vm.makePersistent(mips[i]);
-            }
-            proposals = new TestProposals(mips);
-        } else {
-            bytes memory code = getCode(path);
-            mips[0] = deployCode(code);
-            vm.makePersistent(mips[0]);
-            proposals = new TestProposals(mips);
+            latestProposal.run();
         }
-
-        vm.makePersistent(address(proposals));
-
-        proposals.setUp();
-        proposals.testProposals(
-            false, /// do not log debug output
-            true,
-            true,
-            true,
-            true,
-            true,
-            true,
-            true
-        );
-
-        addresses = proposals.addresses();
 
         /// only etch out precompile contracts if on the moonbeam chain
         if (addresses.isAddressSet("xcUSDT")) {
