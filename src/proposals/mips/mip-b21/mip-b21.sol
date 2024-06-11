a294a3e3390ff780d0c42bd3edece6152dac11d6
Elliot <elliotfriedman3@gmail.com>
2024-06-11 13:52:43 -0700

    MIP-M21: MetaMorpho Vault Activation
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

---
+++ src/proposals/mips/mip-b21/mip-b21.sol
@@ -0,0 +1,221 @@
+//SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.19;
+
+import {Ownable2StepUpgradeable} from "@openzeppelin-contracts-upgradeable/contracts/access/Ownable2StepUpgradeable.sol";
+import {ERC20Upgradeable} from "@openzeppelin-contracts-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
+
+import "@forge-std/Test.sol";
+
+import {Configs} from "@proposals/Configs.sol";
+import {Proposal} from "@proposals/proposalTypes/Proposal.sol";
+import {Addresses} from "@proposals/Addresses.sol";
+import {IStakedWellUplift} from "@protocol/stkWell/IStakedWellUplift.sol";
+import {CrossChainProposal} from "@proposals/proposalTypes/CrossChainProposal.sol";
+import {ParameterValidation} from "@proposals/utils/ParameterValidation.sol";
+import {FeeSplitter as Splitter} from "@protocol/morpho/FeeSplitter.sol";
+
+/// DO_VALIDATE=true DO_PRINT=true DO_BUILD=true DO_RUN=true forge script
+/// src/proposals/mips/mip-b21/mip-b21.sol:mipb21
+contract mipb21 is Proposal, CrossChainProposal, Configs, ParameterValidation {
+    string public constant override name = "MIP-B21";
+
+    uint256 public constant WELL_AMOUNT = 50_000_000 * 1e18;
+
+    /// @notice metamorpho storage slot offset for pending owner
+    bytes32 public constant PENDING_OWNER_SLOT = bytes32(uint256(9));
+
+    constructor() {
+        bytes memory proposalDescription = abi.encodePacked(
+            vm.readFile("./src/proposals/mips/mip-b21/MIP-B21.md")
+        );
+
+        _setProposalDescription(proposalDescription);
+    }
+
+    /// @notice proposal's actions all happen on base
+    function primaryForkId() public view override returns (uint256) {
+        return baseForkId;
+    }
+
+    function deploy(Addresses addresses, address) public override {}
+
+    function afterDeploy(Addresses addresses, address) public override {}
+
+    function afterDeploySetup(Addresses addresses) public override {
+        address usdcMetaMorphoVault = addresses.getAddress(
+            "USDC_METAMORPHO_VAULT"
+        );
+        address wethMetaMorphoVault = addresses.getAddress(
+            "WETH_METAMORPHO_VAULT"
+        );
+        address temporalGovernor = addresses.getAddress("TEMPORAL_GOVERNOR");
+        bytes32 pendingOwner = bytes32(uint256(uint160(temporalGovernor)));
+
+        if (
+            Ownable2StepUpgradeable(usdcMetaMorphoVault).pendingOwner() !=
+            temporalGovernor
+        ) {
+            vm.store(usdcMetaMorphoVault, PENDING_OWNER_SLOT, pendingOwner);
+        }
+        if (
+            Ownable2StepUpgradeable(wethMetaMorphoVault).pendingOwner() !=
+            temporalGovernor
+        ) {
+            vm.store(wethMetaMorphoVault, PENDING_OWNER_SLOT, pendingOwner);
+        }
+    }
+
+    function build(Addresses addresses) public override {
+        address foundationMultisig = addresses.getAddress(
+            "FOUNDATION_MULTISIG"
+        );
+        address temporalGovernor = addresses.getAddress("TEMPORAL_GOVERNOR");
+        ERC20Upgradeable well = ERC20Upgradeable(
+            addresses.getAddress("xWELL_PROXY")
+        );
+
+        if (
+            well.allowance(foundationMultisig, temporalGovernor) < WELL_AMOUNT
+        ) {
+            vm.prank(foundationMultisig);
+            well.approve(temporalGovernor, WELL_AMOUNT);
+        }
+
+        if (well.balanceOf(foundationMultisig) < WELL_AMOUNT) {
+            deal(address(well), foundationMultisig, WELL_AMOUNT);
+        }
+
+        _pushCrossChainAction(
+            addresses.getAddress("USDC_METAMORPHO_VAULT"),
+            abi.encodeWithSignature("acceptOwnership()"),
+            "Accept ownership of the Moonwell USDC Metamorpho Vault"
+        );
+        _pushCrossChainAction(
+            addresses.getAddress("WETH_METAMORPHO_VAULT"),
+            abi.encodeWithSignature("acceptOwnership()"),
+            "Accept ownership of the Moonwell WETH Metamorpho Vault"
+        );
+        _pushCrossChainAction(
+            addresses.getAddress("xWELL_PROXY"),
+            abi.encodeWithSignature(
+                "transferFrom(address,address,uint256)",
+                addresses.getAddress("FOUNDATION_MULTISIG"),
+                addresses.getAddress("MOONWELL_METAMORPHO_URD"),
+                WELL_AMOUNT
+            ),
+            "Transfer 50m WELL from the Foundation Base Multisig to the Moonwell Metamorpho URD"
+        );
+    }
+
+    function teardown(Addresses addresses, address) public pure override {}
+
+    /// @notice assert that the new interest rate model is set correctly
+    /// and that the interest rate model parameters are set correctly
+    function validate(Addresses addresses, address) public override {
+        /// --------------------- WELL ---------------------
+        ERC20Upgradeable well = ERC20Upgradeable(
+            addresses.getAddress("xWELL_PROXY")
+        );
+
+        assertEq(
+            well.balanceOf(addresses.getAddress("MOONWELL_METAMORPHO_URD")),
+            WELL_AMOUNT,
+            "well amount incorrect"
+        );
+        assertEq(
+            well.allowance(
+                addresses.getAddress("FOUNDATION_MULTISIG"),
+                addresses.getAddress("TEMPORAL_GOVERNOR")
+            ),
+            0,
+            "well allowance incorrect"
+        );
+
+        /// --------------------- METAMORPHO VAULTS ---------------------
+
+        /// actual owner
+        assertEq(
+            Ownable2StepUpgradeable(
+                addresses.getAddress("USDC_METAMORPHO_VAULT")
+            ).owner(),
+            addresses.getAddress("TEMPORAL_GOVERNOR"),
+            "USDC Metamorpho Vault ownership incorrect"
+        );
+
+        assertEq(
+            Ownable2StepUpgradeable(
+                addresses.getAddress("WETH_METAMORPHO_VAULT")
+            ).owner(),
+            addresses.getAddress("TEMPORAL_GOVERNOR"),
+            "WETH Metamorpho Vault ownership incorrect"
+        );
+
+        /// pending owner
+        assertEq(
+            Ownable2StepUpgradeable(
+                addresses.getAddress("USDC_METAMORPHO_VAULT")
+            ).pendingOwner(),
+            address(0),
+            "USDC Metamorpho Vault pending owner incorrect"
+        );
+        assertEq(
+            Ownable2StepUpgradeable(
+                addresses.getAddress("WETH_METAMORPHO_VAULT")
+            ).pendingOwner(),
+            address(0),
+            "WETH Metamorpho Vault pending owner incorrect"
+        );
+
+        /// --------------------- SPLITTERS ---------------------
+
+        Splitter wethSplitter = Splitter(
+            addresses.getAddress("WETH_METAMORPHO_FEE_SPLITTER")
+        );
+
+        assertEq(
+            wethSplitter.mToken(),
+            addresses.getAddress("MOONWELL_WETH"),
+            "WETH Metamorpho Fee Splitter fee recipient incorrect"
+        );
+        assertEq(
+            wethSplitter.metaMorphoVault(),
+            addresses.getAddress("WETH_METAMORPHO_VAULT"),
+            "WETH Metamorpho Fee Splitter Vault incorrect"
+        );
+        assertEq(
+            wethSplitter.splitA(),
+            5_000,
+            "WETH Metamorpho Fee Split incorrect"
+        );
+        assertEq(
+            wethSplitter.splitB(),
+            5_000,
+            "WETH Metamorpho Fee Split incorrect"
+        );
+
+        Splitter usdcSplitter = Splitter(
+            addresses.getAddress("USDC_METAMORPHO_FEE_SPLITTER")
+        );
+
+        assertEq(
+            usdcSplitter.mToken(),
+            addresses.getAddress("MOONWELL_USDC"),
+            "USDC Metamorpho Fee Splitter fee recipient incorrect"
+        );
+        assertEq(
+            usdcSplitter.metaMorphoVault(),
+            addresses.getAddress("USDC_METAMORPHO_VAULT"),
+            "USDC Metamorpho Fee Splitter Vault incorrect"
+        );
+        assertEq(
+            usdcSplitter.splitA(),
+            5_000,
+            "USDC Metamorpho Fee Split a incorrect"
+        );
+        assertEq(
+            usdcSplitter.splitB(),
+            5_000,
+            "USDC Metamorpho Fee Split b incorrect"
+        );
+    }
+}
