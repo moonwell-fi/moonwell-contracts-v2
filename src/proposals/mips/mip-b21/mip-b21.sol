175815d7282689a0d2c090fe988fde2866e48e1c
Elliot <elliotfriedman3@gmail.com>
2024-06-11 15:31:29 -0700

    remove unused imports
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- src/proposals/mips/mip-b21/mip-b21.sol
+++ src/proposals/mips/mip-b21/mip-b21.sol
@@ -9,7 +9,6 @@ import "@forge-std/Test.sol";
 import {Configs} from "@proposals/Configs.sol";
 import {Proposal} from "@proposals/proposalTypes/Proposal.sol";
 import {Addresses} from "@proposals/Addresses.sol";
-import {IStakedWellUplift} from "@protocol/stkWell/IStakedWellUplift.sol";
 import {CrossChainProposal} from "@proposals/proposalTypes/CrossChainProposal.sol";
 import {ParameterValidation} from "@proposals/utils/ParameterValidation.sol";
 import {FeeSplitter as Splitter} from "@protocol/morpho/FeeSplitter.sol";
