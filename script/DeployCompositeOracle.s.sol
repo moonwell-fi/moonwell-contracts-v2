d06ef9c6eec2725ac9c65b4b2ed36068574ad4e8
Elliot <elliotfriedman3@gmail.com>
2024-06-20 10:32:19 -0700

    move composite oracle deploy scripts to script folder
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- script/DeployCompositeOracle.s.sol
+++ script/DeployCompositeOracle.s.sol
@@ -4,8 +4,8 @@ pragma solidity 0.8.19;
 import {console} from "@forge-std/console.sol";
 import {Script} from "@forge-std/Script.sol";
 
-import {AllChainAddresses as Addresses} from "@proposals/Addresses.sol";
 import {ChainlinkCompositeOracle} from "@protocol/oracles/ChainlinkCompositeOracle.sol";
+import {AllChainAddresses as Addresses} from "@proposals/Addresses.sol";
 
 /*
 How to use:
