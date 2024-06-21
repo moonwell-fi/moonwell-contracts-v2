8591971bddac58b33fc7029c5027d8e53aa99bf3
Elliot <elliotfriedman3@gmail.com>
2024-06-21 00:44:41 -0700

    order imports
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- test/integration/LiveProposals.t.sol
+++ test/integration/LiveProposals.t.sol
@@ -2,20 +2,21 @@
 pragma solidity 0.8.19;
 
 import "@forge-std/Test.sol";
+
 import {console} from "@forge-std/console.sol";
 
-import {IMultichainGovernor, MultichainGovernor} from "@protocol/governance/multichain/MultichainGovernor.sol";
-import {xWELL} from "@protocol/xWELL/xWELL.sol";
-import {TemporalGovernor} from "@protocol/governance/TemporalGovernor.sol";
-import {IWormhole} from "@protocol/wormhole/IWormhole.sol";
-import {AllChainAddresses as Addresses} from "@proposals/Addresses.sol";
-import {ProposalChecker} from "@proposals/proposalTypes/ProposalChecker.sol";
-import {String} from "@utils/String.sol";
 import {Bytes} from "@utils/Bytes.sol";
+import {xWELL} from "@protocol/xWELL/xWELL.sol";
+import {String} from "@utils/String.sol";
 import {Address} from "@utils/Address.sol";
-import {MIPProposal as Proposal} from "@proposals/MIPProposal.s.sol";
 import {ChainIds} from "@test/utils/ChainIds.sol";
+import {IWormhole} from "@protocol/wormhole/IWormhole.sol";
 import {Implementation} from "@test/mock/wormhole/Implementation.sol";
+import {ProposalChecker} from "@proposals/proposalTypes/ProposalChecker.sol";
+import {TemporalGovernor} from "@protocol/governance/TemporalGovernor.sol";
+import {MIPProposal as Proposal} from "@proposals/MIPProposal.s.sol";
+import {AllChainAddresses as Addresses} from "@proposals/Addresses.sol";
+import {IMultichainGovernor, MultichainGovernor} from "@protocol/governance/multichain/MultichainGovernor.sol";
 
 contract LiveProposalsIntegrationTest is Test, ChainIds, ProposalChecker {
     using String for string;
