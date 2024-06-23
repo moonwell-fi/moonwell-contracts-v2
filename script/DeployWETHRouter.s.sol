984492710fcc995735634d300aad19c1c454c2db
Elliot <elliotfriedman3@gmail.com>
2024-06-22 21:02:51 -0700

    fmt
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- script/DeployWETHRouter.s.sol
+++ script/DeployWETHRouter.s.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.19;
 import {console} from "@forge-std/console.sol";
 import {Script} from "@forge-std/Script.sol";
 
-
 import {WETH9} from "@protocol/router/IWETH.sol";
 import {MErc20} from "@protocol/MErc20.sol";
 import {WETHRouter} from "@protocol/router/WETHRouter.sol";
