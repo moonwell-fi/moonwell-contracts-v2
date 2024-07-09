7b1de96be532b7322db8b36dfd4f40ab1fe7bc67
Elliot <elliotfriedman3@gmail.com>
2024-07-09 14:09:45 -0700

    pass wormhole chain id to all router function calls
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- script/BridgeXWell.s.sol
+++ script/BridgeXWell.s.sol
@@ -4,8 +4,9 @@ import {Script} from "@forge-std/Script.sol";
 
 import "@forge-std/Test.sol";
 
-import {AllChainAddresses as Addresses} from "@proposals/Addresses.sol";
 import {xWELLRouter} from "@protocol/xWELL/xWELLRouter.sol";
+import {BASE_WORMHOLE_CHAIN_ID} from "@utils/ChainIds.sol";
+import {AllChainAddresses as Addresses} from "@proposals/Addresses.sol";
 
 contract BridgeXWell is Script, Test {
     /// @notice addresses contract
@@ -18,13 +19,16 @@ contract BridgeXWell is Script, Test {
     function run() public {
         xWELLRouter router = xWELLRouter(addresses.getAddress("xWELL_ROUTER"));
 
-        uint256 bridgeCost = router.bridgeCost();
+        uint256 bridgeCost = router.bridgeCost(BASE_WORMHOLE_CHAIN_ID);
 
         uint256 amount = 100_000 * 1e18;
 
         vm.startBroadcast();
 
-        router.bridgeToBase{value: bridgeCost}(amount);
+        router.bridgeToSender{value: bridgeCost}(
+            amount,
+            BASE_WORMHOLE_CHAIN_ID
+        );
 
         vm.stopBroadcast();
     }
