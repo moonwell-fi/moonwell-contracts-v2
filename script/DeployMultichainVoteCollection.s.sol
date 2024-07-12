38a9764aa8a08f99dd5ff4ee719b02d294d9febe
solsr <elliotfriedman3@gmail.com>
2024-07-11 19:49:05 -0700

    deployVoteCollection logic and proxy contracts
    Signed-off-by: solsr <elliotfriedman3@gmail.com>

--- script/DeployMultichainVoteCollection.s.sol
+++ script/DeployMultichainVoteCollection.s.sol
@@ -3,7 +3,11 @@ pragma solidity 0.8.19;
 
 import {Script} from "@forge-std/Script.sol";
 
+import "@utils/ChainIds.sol";
+
 import {MultichainVoteCollection} from "@protocol/governance/multichain/MultichainVoteCollection.sol";
+import {MultichainGovernorDeploy} from "@protocol/governance/multichain/MultichainGovernorDeploy.sol";
+import {AllChainAddresses as Addresses} from "@proposals/Addresses.sol";
 
 /*
  Utility to deploy xWELL contract on any network
@@ -15,14 +19,35 @@ import {MultichainVoteCollection} from "@protocol/governance/multichain/Multicha
     forge script script/DeployMultichainVoteCollection.s.sol:DeployMultichainVoteCollection -vvvv \ 
     --rpc-url $chainAlias --broadcast --etherscan-api-key $chainAlias --verify
 */
-contract DeployMultichainVoteCollection is Script {
-    function run() public returns (MultichainVoteCollection) {
+contract DeployMultichainVoteCollection is Script, MultichainGovernorDeploy {
+    using ChainIds for uint256;
+
+    function run() public {
+        Addresses addresses = new Addresses();
+
         vm.startBroadcast();
 
-        MultichainVoteCollection impl = new MultichainVoteCollection();
+        (
+            address collectionProxy,
+            address collectionImpl
+        ) = deployVoteCollection(
+                addresses.getAddress("xWELL_PROXY"),
+                addresses.getAddress("STK_GOVTOKEN"),
+                addresses.getAddress(
+                    "MULTICHAIN_GOVERNOR_PROXY",
+                    block.chainid.toMoonbeamChainId()
+                ),
+                addresses.getAddress("WORMHOLE_BRIDGE_RELAYER"),
+                block.chainid.toMoonbeamWormholeChainId(),
+                addresses.getAddress("MRD_PROXY_ADMIN"),
+                addresses.getAddress("TEMPORAL_GOVERNOR")
+            );
+
+        addresses.addAddress("VOTE_COLLECTION_PROXY", collectionProxy);
+        addresses.addAddress("VOTE_COLLECTION_IMPL", collectionImpl);
 
         vm.stopBroadcast();
 
-        return impl;
+        addresses.printAddresses();
     }
 }
