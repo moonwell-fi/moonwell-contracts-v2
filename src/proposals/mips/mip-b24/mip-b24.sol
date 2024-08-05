c610c1f973101f7636dcc2d89a855535dbb636f7
Sai Srikar <sai@gauntlet.xyz>
2024-08-05 16:11:39 -0500

    RF additions to proposal

--- src/proposals/mips/mip-b24/mip-b24.sol
+++ src/proposals/mips/mip-b24/mip-b24.sol
@@ -11,7 +11,12 @@ import {AllChainAddresses as Addresses} from "@proposals/Addresses.sol";
 
 contract mipb24 is HybridProposal, Configs, ParameterValidation {
     string public constant override name = "MIP-B24";
-
+    uint256 public constant wstETH_NEW_RF = 0.15e18;
+    uint256 public constant rETH_NEW_RF = 0.15e18;
+    uint256 public constant cbETH_NEW_RF = 0.15e18;
+    uint256 public constant USDC_NEW_RF = 0.1e18;
+    uint256 public constant WETH_NEW_RF = 0.15e18;
+    uint256 public constant AERO_NEW_RF = 0.3e18;
     constructor() {
         bytes memory proposalDescription = abi.encodePacked(
             vm.readFile("./src/proposals/mips/mip-b24/MIP-B24.md")
@@ -33,6 +38,45 @@ contract mipb24 is HybridProposal, Configs, ParameterValidation {
 
     function build(Addresses addresses) public override {
         _pushAction(
+            addresses.getAddress("MOONWELL_wstETH"),
+            abi.encodeWithSignature(
+                "_setReserveFactor(uint256)",
+                wstETH_NEW_RF
+            ),
+            "Set reserve factor for Moonwell wstETH to updated reserve factor"
+        );
+
+        _pushAction(
+            addresses.getAddress("MOONWELL_rETH"),
+            abi.encodeWithSignature("_setReserveFactor(uint256)", rETH_NEW_RF),
+            "Set reserve factor for Moonwell rETH to updated reserve factor"
+        );
+
+        _pushAction(
+            addresses.getAddress("MOONWELL_cbETH"),
+            abi.encodeWithSignature("_setReserveFactor(uint256)", cbETH_NEW_RF),
+            "Set reserve factor for Moonwell cbETH to updated reserve factor"
+        );
+
+        _pushAction(
+            addresses.getAddress("MOONWELL_USDC"),
+            abi.encodeWithSignature("_setReserveFactor(uint256)", USDC_NEW_RF),
+            "Set reserve factor for Moonwell USDC to updated reserve factor"
+        );
+
+        _pushAction(
+            addresses.getAddress("MOONWELL_WETH"),
+            abi.encodeWithSignature("_setReserveFactor(uint256)", WETH_NEW_RF),
+            "Set reserve factor for Moonwell WETH to updated reserve factor"
+        );
+
+        _pushAction(
+            addresses.getAddress("MOONWELL_AERO"),
+            abi.encodeWithSignature("_setReserveFactor(uint256)", AERO_NEW_RF),
+            "Set reserve factor for Moonwell AERO to updated reserve factor"
+        );
+
+        _pushAction(
             addresses.getAddress("MOONWELL_AERO"),
             abi.encodeWithSignature(
                 "_setInterestRateModel(address)",
@@ -47,6 +91,18 @@ contract mipb24 is HybridProposal, Configs, ParameterValidation {
     /// @notice assert that the new interest rate model is set correctly
     /// and that the interest rate model parameters are set correctly
     function validate(Addresses addresses, address) public view override {
+        _validateRF(addresses.getAddress("MOONWELL_wstETH"), wstETH_NEW_RF);
+
+        _validateRF(addresses.getAddress("MOONWELL_rETH"), rETH_NEW_RF);
+
+        _validateRF(addresses.getAddress("MOONWELL_cbETH"), cbETH_NEW_RF);
+
+        _validateRF(addresses.getAddress("MOONWELL_USDC"), USDC_NEW_RF);
+
+        _validateRF(addresses.getAddress("MOONWELL_WETH"), WETH_NEW_RF);
+
+        _validateRF(addresses.getAddress("MOONWELL_AERO"), AERO_NEW_RF);
+
         _validateJRM(
             addresses.getAddress("JUMP_RATE_IRM_MOONWELL_AERO_MIP_B24"),
             addresses.getAddress("MOONWELL_AERO"),
