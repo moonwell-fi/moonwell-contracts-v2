8e33c43e793f63fcfc380712a0a6716fd8607e69
Ana Julia <anajuliabit@gmail.com>
2024-08-05 11:41:49 -0300

    update TEMPLATE_PATH due to ambiguios inputs
    https://github.com/foundry-rs/foundry/issues/7641#issuecomment-2051437806

--- src/proposals/mips/mip-o02/o02.sh
+++ src/proposals/mips/mip-o02/o02.sh
@@ -4,7 +4,7 @@ export MIP_REWARDS_PATH=src/proposals/mips/mip-o02/o02.json
 export DESCRIPTION_PATH=src/proposals/mips/mip-o02/o02.md
 export CHAIN_ID=10
 export PRIMARY_FORK_ID=0
-export TEMPLATE_PATH="src/proposals/templates/mipRewardsDistributionExternalChain.sol"
+export TEMPLATE_PATH="src/proposals/templates/mipRewardsDistributionExternalChain.sol:mipRewardsDistributionExternalChain"
 
 # logs template
 echo $TEMPLATE_PATH
