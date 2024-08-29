d10fd9ef481a445d62acd7eb78fd79f1629c26f3
Sai Srikar <sai@gauntlet.xyz>
2024-08-28 21:31:46 -0500

    MIP-O08

---
+++ src/proposals/mips/mip-o08/mip-o08.sol
@@ -0,0 +1,71 @@
+//SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.19;
+
+import "@forge-std/Test.sol";
+
+import {ProposalActions} from "@proposals/utils/ProposalActions.sol";
+import {OPTIMISM_FORK_ID} from "@utils/ChainIds.sol";
+import {ParameterValidation} from "@proposals/utils/ParameterValidation.sol";
+import {HybridProposal, ActionType} from "@proposals/proposalTypes/HybridProposal.sol";
+import {AllChainAddresses as Addresses} from "@proposals/Addresses.sol";
+
+contract mipo03 is HybridProposal, ParameterValidation {
+    using ProposalActions for *;
+
+    string public constant override name = "MIP-O08";
+
+    // Define new reserve and collateral factors for various assets
+    uint256 public constant NEW_WBTC_RESERVE_FACTOR = 0.3e18;
+    uint256 public constant NEW_WBTC_COLLATERAL_FACTOR = 0.79e18;
+
+    constructor() {
+        bytes memory proposalDescription = abi.encodePacked(
+            vm.readFile("./src/proposals/mips/mip-o08/MIP-O08.md")
+        );
+        _setProposalDescription(proposalDescription);
+    }
+
+    function primaryForkId() public pure override returns (uint256) {
+        return OPTIMISM_FORK_ID;
+    }
+
+    function build(Addresses addresses) public override {
+        // Push actions to update Reserve Factors for different assets
+
+        _pushAction(
+            addresses.getAddress("MOONWELL_WBTC"),
+            abi.encodeWithSignature(
+                "_setReserveFactor(uint256)",
+                NEW_WBTC_RESERVE_FACTOR
+            ),
+            "Set reserve factor for Moonwell WBTC to updated reserve factor",
+            ActionType.Optimism
+        );
+
+        _pushAction(
+            addresses.getAddress("UNITROLLER"),
+            abi.encodeWithSignature(
+                "_setCollateralFactor(address,uint256)",
+                addresses.getAddress("MOONWELL_WBTC"),
+                NEW_WBTC_COLLATERAL_FACTOR
+            ),
+            "Set collateral factor of Moonwell WBTC",
+            ActionType.Optimism
+        );
+    }
+
+    function validate(Addresses addresses, address) public view override {
+        // Validate Collateral Factors
+
+        _validateCF(
+            addresses,
+            addresses.getAddress("MOONWELL_WBTC"),
+            NEW_WBTC_COLLATERAL_FACTOR
+        );
+
+        _validateRF(
+            addresses.getAddress("MOONWELL_WBTC"),
+            NEW_WBTC_RESERVE_FACTOR
+        );
+    }
+}
