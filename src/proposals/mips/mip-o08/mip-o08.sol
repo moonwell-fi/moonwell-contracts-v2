d7cfd694e8f1d08462892662f294ceac9da0f7a5
Ana Julia <anajuliabit@gmail.com>
2024-08-30 15:11:23 -0300

    Add JRM deployment

--- src/proposals/mips/mip-o08/mip-o08.sol
+++ src/proposals/mips/mip-o08/mip-o08.sol
@@ -8,6 +8,7 @@ import {OPTIMISM_FORK_ID} from "@utils/ChainIds.sol";
 import {ParameterValidation} from "@proposals/utils/ParameterValidation.sol";
 import {HybridProposal, ActionType} from "@proposals/proposalTypes/HybridProposal.sol";
 import {AllChainAddresses as Addresses} from "@proposals/Addresses.sol";
+import {JumpRateModel, InterestRateModel} from "@protocol/irm/JumpRateModel.sol";
 
 contract mipo08 is HybridProposal, ParameterValidation {
     using ProposalActions for *;
@@ -18,11 +19,24 @@ contract mipo08 is HybridProposal, ParameterValidation {
     uint256 public constant NEW_WBTC_RESERVE_FACTOR = 0.3e18;
     uint256 public constant NEW_WBTC_COLLATERAL_FACTOR = 0.79e18;
 
+    uint256 public constant BASE_RATE = 0;
+    uint256 public constant KINK = 0.9e18;
+    uint256 public constant MULTIPLIER = 0.056e18;
+    uint256 public constant JUMP_MULTIPLIER = 5e18;
+
     constructor() {
         bytes memory proposalDescription = abi.encodePacked(
             vm.readFile("./src/proposals/mips/mip-o08/MIP-O08.md")
         );
         _setProposalDescription(proposalDescription);
+    }
+
+    function deploy(Addresses addresses, address) public override {
+        address irModel = address(
+            new JumpRateModel(BASE_RATE, MULTIPLIER, JUMP_MULTIPLIER, KINK)
+        );
+
+        addresses.addAddress("JUMP_RATE_IRM_MOONWELL_MIP_O08", irModel);
     }
 
     function primaryForkId() public pure override returns (uint256) {
@@ -57,7 +71,7 @@ contract mipo08 is HybridProposal, ParameterValidation {
             addresses.getAddress("MOONWELL_USDC"),
             abi.encodeWithSignature(
                 "_setInterestRateModel(address)",
-                addresses.getAddress("JUMP_RATE_IRM_MOONWELL_USDC_MIP_O08")
+                addresses.getAddress("JUMP_RATE_IRM_MOONWELL_MIP_O08")
             ),
             "Set interest rate model for Moonwell USDC to updated rate model"
         );
@@ -66,7 +80,7 @@ contract mipo08 is HybridProposal, ParameterValidation {
             addresses.getAddress("MOONWELL_USDT"),
             abi.encodeWithSignature(
                 "_setInterestRateModel(address)",
-                addresses.getAddress("JUMP_RATE_IRM_MOONWELL_USDT_MIP_O08")
+                addresses.getAddress("JUMP_RATE_IRM_MOONWELL_MIP_O08")
             ),
             "Set interest rate model for Moonwell USDT to updated rate model"
         );
@@ -75,7 +89,7 @@ contract mipo08 is HybridProposal, ParameterValidation {
             addresses.getAddress("MOONWELL_DAI"),
             abi.encodeWithSignature(
                 "_setInterestRateModel(address)",
-                addresses.getAddress("JUMP_RATE_IRM_MOONWELL_DAI_MIP_O08")
+                addresses.getAddress("JUMP_RATE_IRM_MOONWELL_MIP_O08")
             ),
             "Set interest rate model for Moonwell DAI to updated rate model"
         );
@@ -96,35 +110,35 @@ contract mipo08 is HybridProposal, ParameterValidation {
         );
 
         _validateJRM(
-            addresses.getAddress("JUMP_RATE_IRM_MOONWELL_USDC_MIP_O08"),
+            addresses.getAddress("JUMP_RATE_IRM_MOONWELL_MIP_O08"),
             addresses.getAddress("MOONWELL_USDC"),
             IRParams({
-                baseRatePerTimestamp: 0,
-                kink: 0.9e18,
-                multiplierPerTimestamp: 0.056e18,
-                jumpMultiplierPerTimestamp: 5e18
+                baseRatePerTimestamp: BASE_RATE,
+                kink: KINK,
+                multiplierPerTimestamp: MULTIPLIER,
+                jumpMultiplierPerTimestamp: JUMP_MULTIPLIER
             })
         );
 
         _validateJRM(
-            addresses.getAddress("JUMP_RATE_IRM_MOONWELL_USDT_MIP_O08"),
+            addresses.getAddress("JUMP_RATE_IRM_MOONWELL_MIP_O08"),
             addresses.getAddress("MOONWELL_USDT"),
             IRParams({
-                baseRatePerTimestamp: 0,
-                kink: 0.9e18,
-                multiplierPerTimestamp: 0.056e18,
-                jumpMultiplierPerTimestamp: 5e18
+                baseRatePerTimestamp: BASE_RATE,
+                kink: KINK,
+                multiplierPerTimestamp: MULTIPLIER,
+                jumpMultiplierPerTimestamp: JUMP_MULTIPLIER
             })
         );
 
         _validateJRM(
-            addresses.getAddress("JUMP_RATE_IRM_MOONWELL_DAI_MIP_O08"),
+            addresses.getAddress("JUMP_RATE_IRM_MOONWELL_MIP_O08"),
             addresses.getAddress("MOONWELL_DAI"),
             IRParams({
-                baseRatePerTimestamp: 0,
-                kink: 0.9e18,
-                multiplierPerTimestamp: 0.056e18,
-                jumpMultiplierPerTimestamp: 5e18
+                baseRatePerTimestamp: BASE_RATE,
+                kink: KINK,
+                multiplierPerTimestamp: MULTIPLIER,
+                jumpMultiplierPerTimestamp: JUMP_MULTIPLIER
             })
         );
     }
