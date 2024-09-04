8c745c0692cf03b9dae1b572d55d5e714366b828
solsr <elliotfriedman3@gmail.com>
2024-09-04 16:39:38 -0700

    refactor: use Address contract printAddresses method
    Signed-off-by: solsr <elliotfriedman3@gmail.com>

--- src/proposals/mips/mip-xwell/xwellDeployMoonbeam.sol
+++ src/proposals/mips/mip-xwell/xwellDeployMoonbeam.sol
@@ -150,7 +150,7 @@ contract xwellDeployMoonbeam is Proposal, Configs, xWELLDeploy {
             addresses.addAddress("xWELL_LOGIC", xwellLogic);
             addresses.addAddress("xWELL_PROXY", xwellProxy);
 
-            printAddresses(addresses);
+            addresses.printAddresses();
             addresses.resetRecordingAddresses();
         }
     }
@@ -282,23 +282,6 @@ contract xwellDeployMoonbeam is Proposal, Configs, xWELLDeploy {
                 xWELL(moonbeamxWellProxy).pauseGuardian(),
                 pauseGuardian,
                 "pause guardian address is incorrect"
-            );
-        }
-    }
-
-    function printAddresses(Addresses addresses) private view {
-        (
-            string[] memory recordedNames,
-            ,
-            address[] memory recordedAddresses
-        ) = addresses.getRecordedAddresses();
-        for (uint256 j = 0; j < recordedNames.length; j++) {
-            console.log("{\n        'addr': '%s', ", recordedAddresses[j]);
-            console.log("        'chainId': %d,", block.chainid);
-            console.log(
-                "        'name': '%s'\n}%s",
-                recordedNames[j],
-                j < recordedNames.length - 1 ? "," : ""
             );
         }
     }
