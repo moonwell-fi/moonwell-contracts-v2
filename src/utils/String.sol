4c167dd93fe59b5f24deb23d64adbe69a8e3ae60
Ana Julia <anajuliabit@gmail.com>
2024-09-13 12:06:18 -0300

    lint

--- src/utils/String.sol
+++ src/utils/String.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.19;
 
-import {console} from "@forge-std/console.sol";
-
 library String {
     function hasChar(
         string memory _string,
