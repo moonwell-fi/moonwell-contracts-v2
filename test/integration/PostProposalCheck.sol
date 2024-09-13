3d4f10f16f9cee377f0b13547331f102c7cef78c
Ana Julia <anajuliabit@gmail.com>
2024-09-13 14:27:54 -0300

    remove proposals length initialization

--- test/integration/PostProposalCheck.sol
+++ test/integration/PostProposalCheck.sol
@@ -51,8 +51,6 @@ contract PostProposalCheck is LiveProposalCheck {
             return;
         }
 
-        proposals = new Proposal[](devProposals.length);
-
         // execute in the inverse order so that the lowest id is executed first
         for (uint256 i = devProposals.length; i > 0; i--) {
             proposalMap.executeShellFile(devProposals[i - 1].envPath);
