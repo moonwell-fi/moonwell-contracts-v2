ed386f1fa7ef2c1651963dd4d180022f0caba433
Ana Julia <anajuliabit@gmail.com>
2024-09-18 15:02:02 -0300

    Merge mipRewardsDistribution proposal template back

--- src/proposals/templates/mipRewardsDistributionMoonbeam.sol
+++ src/proposals/templates/mipRewardsDistributionMoonbeam.sol
@@ -284,6 +284,7 @@ contract mipRewardsDistributionMoonbeam is HybridProposal, Networks {
                 )
             );
         }
+
         _pushAction(
             addresses.getAddress("STK_GOVTOKEN"),
             abi.encodeWithSignature(
@@ -300,6 +301,7 @@ contract mipRewardsDistributionMoonbeam is HybridProposal, Networks {
             ),
             ActionType.Moonbeam
         );
+
         for (uint256 i = 0; i < spec.setRewardSpeed.length; i++) {
             SetRewardSpeed memory setRewardSpeed = spec.setRewardSpeed[i];
             _pushAction(
@@ -403,11 +405,13 @@ contract mipRewardsDistributionMoonbeam is HybridProposal, Networks {
             0,
             "xWELL Router should not have an open allowance after execution"
         );
+
         address stkGovToken = addresses.getAddress("STK_GOVTOKEN");
         // assert safety module reward speed
         IStakedWell stkWell = IStakedWell(stkGovToken);
         (uint256 emissionsPerSecond, , ) = stkWell.assets(stkGovToken);
         assertEq(emissionsPerSecond, spec.stkWellEmissionsPerSecond);
+
         // validate setRewardSpeed calls
         for (uint256 i = 0; i < spec.setRewardSpeed.length; i++) {
             SetRewardSpeed memory setRewardSpeed = spec.setRewardSpeed[i];
