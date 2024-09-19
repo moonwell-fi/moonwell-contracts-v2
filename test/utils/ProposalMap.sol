4e4da24379fa1a755aaf478c34ab855e66b77ae9
Ana Julia <anajuliabit@gmail.com>
2024-09-18 21:06:34 -0300

    compare hashes

--- test/utils/ProposalMap.sol
+++ test/utils/ProposalMap.sol
@@ -104,8 +104,8 @@ contract ProposalMap is Test {
         uint256 count = 0;
         for (uint256 i = 0; i < proposals.length; i++) {
             if (
-                bytes32(bytes(proposals[i].governor)) ==
-                bytes32(bytes(governor))
+                keccak256(abi.encodePacked(proposals[i].governor)) ==
+                keccak256(abi.encodePacked(governor))
             ) {
                 count++;
             }
@@ -115,8 +115,8 @@ contract ProposalMap is Test {
         uint256 index = 0;
         for (uint256 i = 0; i < proposals.length; i++) {
             if (
-                bytes32(bytes(proposals[i].governor)) ==
-                bytes32(bytes(governor))
+                keccak256(abi.encodePacked(proposals[i].governor)) ==
+                keccak256(abi.encodePacked(governor))
             ) {
                 _proposals[index] = proposals[i];
                 index++;
