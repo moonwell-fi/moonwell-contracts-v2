63e35db1f353ddb826fa429bc7e16437d833f5dc
Ana Julia <anajuliabit@gmail.com>
2024-09-27 19:00:31 -0300

    build actions

--- src/proposals/templates/marketRecommendations.sol
+++ src/proposals/templates/marketRecommendations.sol
@@ -4,11 +4,15 @@ pragma solidity 0.8.19;
 import "@forge-std/Test.sol";
 import "@forge-std/StdJson.sol";
 
+import "@protocol/utils/ChainIds.sol";
 import {Networks} from "@proposals/utils/Networks.sol";
+import {AllChainAddresses as Addresses} from "@proposals/Addresses.sol";
 import {HybridProposal, ActionType} from "@proposals/proposalTypes/HybridProposal.sol";
 
 contract MarketRecommendationsTemplate is HybridProposal, Networks {
     using stdJson for string;
+    using ChainIds for uint256;
+    using stdStorage for StdStorage;
 
     struct MarketUpdate {
         uint256 collateralFactor;
@@ -31,14 +35,21 @@ contract MarketRecommendationsTemplate is HybridProposal, Networks {
         return "MIP Market Recommendations Update";
     }
 
+    function name() external pure override returns (string memory) {
+        return "MIP Rewards Distribution";
+    }
+
+    function primaryForkId() public pure override returns (uint256) {
+        return MOONBEAM_FORK_ID;
+    }
+
     function initProposal(Addresses addresses) public override {
         string memory encodedJson = vm.readFile(vm.envString("JSON_PATH"));
 
         for (uint256 i = 0; i < networks.length; i++) {
             uint256 chainId = networks[i].chainId;
-            string memory data = encodedJson.get(networks[i].name);
 
-            _saveChainMarketUpdate(addresses, chainId, data);
+            _saveChainMarketUpdate(addresses, chainId, encodedJson);
         }
     }
 
@@ -62,6 +73,64 @@ contract MarketRecommendationsTemplate is HybridProposal, Networks {
                 vm.toString(update.collateralFactor),
                 update.irm
             );
+
+            marketUpdates[chainId].push(update);
+        }
+    }
+
+    function _buildChainActions(Addresses addresses, uint256 chainId) {
+        vm.selectFork(chainId.toForkId());
+        MarketUpdate[] memory updates = marketUpdates[chainId];
+
+        for (uint256 i = 0; i < updates.length; i++) {
+            MarketUpdate memory rec = updates[i];
+            if (rec.collateralFactor != -1) {
+                _pushAction(
+                    addresses.getAddress(rec.market),
+                    abi.encodeWithSignature(
+                        "_setCollateralFactor(uint256)",
+                        rec.collateralFactor
+                    ),
+                    string(
+                        abi.encodePacked(
+                            "Set collateral factor for ",
+                            rec.market
+                        )
+                    )
+                );
+            }
+
+            if (rec.reserveFactor != -1) {
+                _pushAction(
+                    addresses.getAddress(rec.market),
+                    abi.encodeWithSignature(
+                        "_setReserveFactor(uint256)",
+                        rec.reserveFactor
+                    ),
+                    string(
+                        abi.encodePacked("Set reserve factor for ", rec.market)
+                    )
+                );
+            }
+
+            if (keccak256(abi.encodePacked(rec.irm)) != keccak256("")) {
+                _pushAction(
+                    addresses.getAddress(rec.market),
+                    abi.encodeWithSignature(
+                        "_setInterestRateModel(string)",
+                        rec.irm
+                    ),
+                    string(abi.encodePacked("Set IRM for ", rec.market))
+                );
+            }
+        }
+    }
+
+    function validate(Addresses addresses, address) public view override {
+        for (uint256 i = 0; i < networks.length; i++) {
+            uint256 chainId = networks[i].chainId;
+
+            //_validateChain(addresses, chainId);
         }
     }
 }
