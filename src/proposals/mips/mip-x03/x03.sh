550610906eabc28dd91cdc8e932ba2a5b6e93d3b
Ana Julia <anajuliabit@gmail.com>
2024-10-07 18:08:23 -0300

    add x03

---
+++ src/proposals/mips/mip-x03/x03.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export MTOKENS_PATH=src/proposals/mips/mip-x03/MTokens.json
+echo "MTOKENS_PATH=$MTOKENS_PATH"
+
+export EMISSION_CONFIGURATIONS_PATH=src/proposals/mips/mip-x03/RewardStreams.json
+
+export DESCRIPTION_PATH=src/proposals/mips/mip-x02/x02.md
+echo "DESCRIPTION_PATH=$DESCRIPTION_PATH"
+
+export PRIMARY_FORK_ID=0
+echo "PRIMARY_FORK_ID=$PRIMARY_FORK_ID"
+
