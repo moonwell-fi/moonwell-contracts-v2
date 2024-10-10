5c4ce4be59795078fd8998d54f9ae673eb4a70f5
Ana Julia <anajuliabit@gmail.com>
2024-10-10 18:56:57 -0300

    delete b33

--- src/proposals/mips/mip-x03/x03.sh
+++ src/proposals/mips/mip-x03/x03.sh
@@ -5,7 +5,7 @@ echo "MTOKENS_PATH=$MTOKENS_PATH"
 export EMISSION_CONFIGURATIONS_PATH=src/proposals/mips/mip-x03/RewardStreams.json
 echo "EMISSION_CONFIGURATIONS_PATH=$EMISSION_CONFIGURATIONS_PATH"
 
-export DESCRIPTION_PATH=src/proposals/mips/mip-x02/x02.md
+export DESCRIPTION_PATH=src/proposals/mips/mip-x02/MIP-X03.md
 echo "DESCRIPTION_PATH=$DESCRIPTION_PATH"
 
 export PRIMARY_FORK_ID=0
