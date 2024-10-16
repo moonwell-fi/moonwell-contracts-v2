705ba109451e0df6da2e6647f8ade743aa879b2c
Ana Julia <anajuliabit@gmail.com>
2024-10-16 17:25:14 -0300

    update ProposalView.sol

--- src/views/ProposalView.sol
+++ src/views/ProposalView.sol
@@ -2,20 +2,36 @@
 pragma solidity 0.8.19;
 
 contract ProposalView {
-    address public relayer;
+    address public immutable relayer;
+
+    /// Uknown can be used to represent a non-existent proposal,
+    /// or a proposal that has not been queued/executed yet.
+    enum ProposalState {
+        Unknown,
+        Queued,
+        Executed
+    }
+
+    mapping(uint256 proposalId => ProposalState state) public proposalStates;
+
+    event ProposalStateChanged(uint256 indexed proposalId, ProposalState state);
 
     constructor(address _relayer) {
         relayer = _relayer;
     }
 
-    enum ProposalState {
-        Queued,
-        Executed
-    }
+    /// @notice Function called by the relayer to notify a proposal state change.
+    function updateProposalState(
+        uint256 proposalId,
+        ProposalState state
+    ) external {
+        require(
+            msg.sender == relayer,
+            "ProposalView: only relayer can update state"
+        );
 
-    event ProposalStateChanged(uint256 indexed proposalId, ProposalState state);
+        proposalStates[proposalId] = state;
 
-    function emitEvent(uint256 proposalId, ProposalState state) external {
         emit ProposalStateChanged(proposalId, state);
     }
 }
