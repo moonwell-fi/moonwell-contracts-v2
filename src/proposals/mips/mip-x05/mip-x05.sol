c388b573e1df7d9b5418b2a8e616fd10d7c4968f
Ana Julia <anajuliabit@gmail.com>
2024-10-22 17:25:29 -0300

    Add mip x05

---
+++ src/proposals/mips/mip-x05/mip-x05.sol
@@ -0,0 +1,45 @@
+//SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.19;
+
+import "@utils/ChainIds.sol";
+import {HybridProposal, ActionType} from "@proposals/proposalTypes/HybridProposal.sol";
+
+contract mipx05 is HybridProposal {
+    string public constant override name = "MIP-X05";
+
+    uint256 public constant COOLDOWN_SECONDS = 7 days;
+
+    constructor() {
+        bytes memory proposalDescription = abi.encodePacked(
+            vm.readFile("./src/proposals/mips/mip-x05/x05.md")
+        );
+
+        _setProposalDescription(proposalDescription);
+    }
+
+    function primaryForkId() public pure override returns (uint256) {
+        return MOONBEAM_FORK_ID;
+    }
+
+    function build(Addresses addresses) public override {
+        _pushAction(
+            addresses.getAddress("STK_GOVTOKEN_PROXY", BASE_CHAIN_ID),
+            abi.encodeWithSignature(
+                "setCoolDownSeconds(uint256)",
+                COOLDOWN_SECONDS
+            ),
+            "Set the cooldown period for stkWELL on Base",
+            ActionType.Base
+        );
+
+        _pushAction(
+            addresses.getAddress("STK_GOVTOKEN_PROXY", OPTIMISM_CHAIN_ID),
+            abi.encodeWithSignature(
+                "setCoolDownSeconds(uint256)",
+                COOLDOWN_SECONDS
+            ),
+            "Set the cooldown period for stkWELL on Base",
+            ActionType.Optimism
+        );
+    }
+}
