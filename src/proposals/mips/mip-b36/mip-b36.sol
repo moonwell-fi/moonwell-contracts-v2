d21b3953ea28904ee443b792f6f1cd1f7a44a88c
Ana Julia <anajuliabit@gmail.com>
2024-10-30 17:31:47 -0300

    add mip-b36

---
+++ src/proposals/mips/mip-b36/mip-b36.sol
@@ -0,0 +1,54 @@
+//SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.19;
+
+import "@forge-std/Test.sol";
+
+import {Configs} from "@proposals/Configs.sol";
+import {BASE_FORK_ID} from "@utils/ChainIds.sol";
+import {HybridProposal} from "@proposals/proposalTypes/HybridProposal.sol";
+import {AllChainAddresses as Addresses} from "@proposals/Addresses.sol";
+
+import {IMetaMorpho} from "@protocol/morpho/IMetaMorpho.sol";
+
+contract mipb36 is HybridProposal, Configs {
+    string public constant override name = "MIP-B36";
+
+    constructor() {
+        bytes memory proposalDescription = abi.encodePacked(
+            vm.readFile("./src/proposals/mips/mip-b36/MIP-B36.md")
+        );
+        _setProposalDescription(proposalDescription);
+    }
+
+    function primaryForkId() public pure override returns (uint256) {
+        return BASE_FORK_ID;
+    }
+
+    function deploy(Addresses addresses, address) public override {}
+
+    function build(Addresses addresses) public override {
+        _pushAction(
+            addresses.getAddress("EURC_METAMORPHO_VAULT"),
+            abi.encodeWithSignature(
+                "setIsAllocator",
+                addresses.getAddress("MORPHO_PUBLIC_ALLOCATOR")
+            ),
+            "Set interest rate model for Moonwell WETH to updated rate model"
+        );
+    }
+
+    function teardown(Addresses addresses, address) public pure override {}
+
+    /// @notice assert that the new interest rate model is set correctly
+    /// and that the interest rate model parameters are set correctly
+    function validate(Addresses addresses, address) public view override {
+        IMetaMorpho vault = IMetaMorpho(
+            addresses.getAddress("EURC_METAMORPHO_VAULT")
+        );
+
+        assertEq(
+            vault.isAllocator(addresses.getAddress("MORPHO_PUBLIC_ALLOCATOR")),
+            true
+        );
+    }
+}
