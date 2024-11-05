42e27bdb358ad10e932b1eee0f5d2ad0e841b724
Ana Julia <anajuliabit@gmail.com>
2024-11-05 17:52:59 -0300

    add support for moonwell allowance transfer contract

--- src/cypher/CypherAutoLoad.sol
+++ src/cypher/CypherAutoLoad.sol
@@ -1,19 +1,15 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.20;
+pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts/utils/Pausable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+import {IAllowanceTransfer} from "./IAllowanceTransfer.sol";
 
 contract CypherAutoLoad is Pausable, AccessControl, ReentrancyGuard {
-    using SafeERC20 for IERC20;
-
     bytes32 public constant EXECUTIONER_ROLE = keccak256("EXECUTIONER_ROLE");
     address public beneficiary;
+    IAllowanceTransfer public moonwellAllowanceTransfer;
 
     event Withdraw(
         address indexed token,
@@ -47,7 +43,6 @@ contract CypherAutoLoad is Pausable, AccessControl, ReentrancyGuard {
      * @dev Only the EXECUTIONER_ROLE can call this function.
      * @param tokenAddress The address of the token to be debited.
      * @param userAddress The address of the user from whom tokens will be debited.
-     * @param beneficiaryAddress The address of the beneficiary receiving the tokens.
      * @param amount The amount of tokens to debit and transfer.
      * @return A boolean indicating the success of the transfer.
      */
@@ -55,18 +50,18 @@ contract CypherAutoLoad is Pausable, AccessControl, ReentrancyGuard {
         address tokenAddress,
         address userAddress,
         uint amount
-    )
-        external
-        whenNotPaused
-        nonReentrant
-        onlyRole(EXECUTIONER_ROLE)
-        returns (bool)
-    {
+    ) external whenNotPaused onlyRole(EXECUTIONER_ROLE) returns (bool) {
         require(userAddress != address(0), "Invalid user address");
+        require(tokenAddress != address(0), "Invalid token address");
 
         IERC20 token = IERC20(tokenAddress);
 
-        token.safeTransferFrom(userAddress, beneficiary, amount);
+        moonwellAllowanceTransfer.transferFrom(
+            userAddress,
+            beneficiary,
+            amount,
+            tokenAddress
+        );
         emit Withdraw(tokenAddress, userAddress, beneficiary, amount);
         return true;
     }
