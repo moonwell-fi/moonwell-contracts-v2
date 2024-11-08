3d22f176472ec4fa5d45f97194b277b63f63e4ef
Ana Julia <anajuliabit@gmail.com>
2024-11-07 21:17:17 -0300

    Cypher auto load natspec comments

--- src/cypher/CypherAutoLoad.sol
+++ src/cypher/CypherAutoLoad.sol
@@ -6,11 +6,21 @@ import "@openzeppelin-contracts/contracts/access/AccessControlEnumerable.sol";
 
 import {IRateLimitedAllowance} from "./IRateLimitedAllowance.sol";
 
+/// @title CypherAutoLoad
+/// @notice A contract for managing token transfers with rate-limited allowances
+/// @dev Inherits from Pausable and AccessControlEnumerable
 contract CypherAutoLoad is Pausable, AccessControlEnumerable {
+    /// @notice the executioner role
     bytes32 public constant EXECUTIONER_ROLE = keccak256("EXECUTIONER_ROLE");
 
+    /// @notice the beneficiary who will receive th etokens
     address public beneficiary;
 
+    /// @notice Emitted when tokens are withdrawn
+    /// @param token The address of the token being withdrawn
+    /// @param user The address of the user from whom tokens are withdrawn
+    /// @param beneficiary The address receiving the withdrawn tokens
+    /// @param amount The amount of tokens withdrawn
     event Withdraw(
         address indexed token,
         address indexed user,
@@ -18,8 +28,13 @@ contract CypherAutoLoad is Pausable, AccessControlEnumerable {
         uint amount
     );
 
+    /// @notice Emitted when the beneficiary address is changed
+    /// @param _beneficiary The new beneficiary address
     event BeneficiaryChanged(address _beneficiary);
 
+    /// @notice Contract constructor
+    /// @param _executioner Address to be granted the EXECUTIONER_ROLE
+    /// @param _beneficiary Initial beneficiary address
     constructor(address _executioner, address _beneficiary) {
         beneficiary = _beneficiary;
         _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
@@ -27,14 +42,12 @@ contract CypherAutoLoad is Pausable, AccessControlEnumerable {
         _setRoleAdmin(EXECUTIONER_ROLE, DEFAULT_ADMIN_ROLE);
     }
 
-    /**
-     * @notice Debit tokens from a user's account and transfer to a beneficiary.
-     * @dev Only the EXECUTIONER_ROLE can call this function.
-     * *param rateLimitedAllowance The rate limited allowance contract.
-     * @param tokenAddress The address of the token to be debited.
-     * @param userAddress The address of the user from whom tokens will be debited.
-     * @param amount The amount of tokens to debit and transfer.
-     */
+    /// @notice Debit tokens from a user's account and transfer to the beneficiary
+    /// @dev Only callable by addresses with EXECUTIONER_ROLE when the contract is not paused
+    /// @param rateLimitedAllowance The address of the rate-limited allowance contract
+    /// @param tokenAddress The address of the token to be debited
+    /// @param userAddress The address of the user from whom tokens will be debited
+    /// @param amount The amount of tokens to debit and transfer
     function debit(
         address rateLimitedAllowance,
         address tokenAddress,
@@ -58,6 +71,8 @@ contract CypherAutoLoad is Pausable, AccessControlEnumerable {
         emit Withdraw(tokenAddress, userAddress, beneficiary, amount);
     }
 
+    /// @notice Pause the contract
+    /// @dev Can be called by addresses with EXECUTIONER_ROLE or DEFAULT_ADMIN_ROLE
     function pause() external whenNotPaused {
         require(
             hasRole(EXECUTIONER_ROLE, msg.sender) ||
@@ -67,10 +82,15 @@ contract CypherAutoLoad is Pausable, AccessControlEnumerable {
         _pause();
     }
 
+    /// @notice Unpause the contract
+    /// @dev Can only be called by addresses with DEFAULT_ADMIN_ROLE when the contract is paused
     function unpause() external whenPaused onlyRole(DEFAULT_ADMIN_ROLE) {
         _unpause();
     }
 
+    /// @notice Set a new beneficiary address
+    /// @dev Can only be called by addresses with DEFAULT_ADMIN_ROLE
+    /// @param _beneficiary The new beneficiary address to be set
     function setBeneficiary(
         address _beneficiary
     ) external onlyRole(DEFAULT_ADMIN_ROLE) {
