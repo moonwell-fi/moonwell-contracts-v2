a93291f6eff1de9238c0ddf953b109e5704b5179
Ana Julia <anajuliabit@gmail.com>
2024-11-07 21:24:08 -0300

    RateLimitedAllowance natspec comments

--- src/cypher/RateLimitedAllowance.sol
+++ src/cypher/RateLimitedAllowance.sol
@@ -7,10 +7,18 @@ import {Pausable} from "@openzeppelin-contracts/contracts/security/Pausable.sol"
 import {RateLimitedLibrary, RateLimit} from "@zelt/src/lib/RateLimitedLibrary.sol";
 import {RateLimitCommonLibrary} from "@zelt/src/lib/RateLimitCommonLibrary.sol";
 
+/// @title Rate Limited Allowance Contract
+/// @notice This contract implements a rate-limited allowance mechanism for token transfers
+/// @dev Inherits from Pausable and Ownable
 abstract contract RateLimitedAllowance is Pausable, Ownable {
     using RateLimitedLibrary for RateLimit;
     using RateLimitCommonLibrary for RateLimit;
 
+    /// @notice Emitted when a new allowance is approved
+    /// @param token The address of the token
+    /// @param owner The address of the owner
+    /// @param rateLimitPerSecond The rate limit per second
+    /// @param bufferCap The buffer cap
     event Approved(
         address indexed token,
         address indexed owner,
@@ -18,18 +26,29 @@ abstract contract RateLimitedAllowance is Pausable, Ownable {
         uint128 bufferCap
     );
 
+    /// @notice Emitted when the spender address is changed
+    /// @param newSpender The address of the new spender
     event SpenderChanged(address newSpender);
 
+    /// @notice The address of the authorized spender
     address public spender;
 
+    /// @notice Mapping of owner to token to rate limit
     mapping(address owner => mapping(address token => RateLimit))
         public limitedAllowance;
 
+    /// @notice Constructor to initialize the contract
+    /// @param owner The address of the contract owner
+    /// @param _spender The address of the authorized spender
     constructor(address owner, address _spender) Ownable() {
         spender = _spender;
         _transferOwnership(owner);
     }
 
+    /// @notice Approves a rate-limited allowance for a token
+    /// @param token The address of the token
+    /// @param rateLimitPerSecond The rate limit per second
+    /// @param bufferCap The buffer cap
     function approve(
         address token,
         uint128 rateLimitPerSecond,
@@ -51,6 +70,11 @@ abstract contract RateLimitedAllowance is Pausable, Ownable {
         emit Approved(token, msg.sender, rateLimitPerSecond, bufferCap);
     }
 
+    /// @notice Transfers tokens from one address to another, respecting the rate limit
+    /// @param from The address to transfer from
+    /// @param to The address to transfer to
+    /// @param amount The amount of tokens to transfer
+    /// @param token The address of the token to transfer
     function transferFrom(
         address from,
         address to,
@@ -65,6 +89,11 @@ abstract contract RateLimitedAllowance is Pausable, Ownable {
         _transfer(from, to, amount, token);
     }
 
+    /// @notice Gets the rate-limited allowance for a token
+    /// @param owner The address of the owner
+    /// @param token The address of the token
+    /// @return rateLimitPerSecond The rate limit per second
+    /// @return bufferCap the bufferCap
     function getRateLimitedAllowance(
         address owner,
         address token
@@ -75,19 +104,27 @@ abstract contract RateLimitedAllowance is Pausable, Ownable {
         bufferCap = limit.bufferCap;
     }
 
+    /// @notice Pauses the contract
+    /// @dev Can only be called by the contract owner when the contract is not paused
     function pause() external whenNotPaused onlyOwner {
         _pause();
     }
 
-    function unpause() external whenPaused onlyOwner {
-        _unpause();
-    }
+    /// @notice Unpauses the contract
+    /// @dev Can only be called by the contract owner when the contract is paused
+    function unpause() external whenPaused onlyOwner {}
 
-    function setSpender(address _spender) external onlyOwner {
-        spender = _spender;
-        emit SpenderChanged(_spender);
-    }
+    /// @notice Sets a new authorized spender address
+    /// @dev Can only be called by the contract owner
+    /// @param _spender The address of the new spender
+    function setSpender(address _spender) external onlyOwner {}
 
+    /// @notice Internal function to transfer tokens
+    /// @dev This function should be implemented by the inheriting contract
+    /// @param from The address to transfer from
+    /// @param to The address to transfer to
+    /// @param amount The amount of tokens to transfer
+    /// @param token The address of the token to transfer
     function _transfer(
         address from,
         address to,
