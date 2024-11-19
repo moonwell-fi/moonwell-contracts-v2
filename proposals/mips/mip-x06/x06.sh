418e9c810b251b70c0a7d445b987959aa9156499
Ana Julia <anajuliabit@gmail.com>
2024-11-19 11:19:40 -0300

    update files path

--- proposals/mips/mip-x06/x06.sh
+++ proposals/mips/mip-x06/x06.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export MIP_REWARDS_PATH=src/proposals/mips/mip-x06/x06.json
+export MIP_REWARDS_PATH=proposals/mips/mip-x06/x06.json
 echo "MIP_REWARDS_PATH=$MIP_REWARDS_PATH"
 
-export DESCRIPTION_PATH=src/proposals/mips/mip-x06/x06.md
+export DESCRIPTION_PATH=proposals/mips/mip-x06/x06.md
 echo "DESCRIPTION_PATH=$DESCRIPTION_PATH"
 
 export PRIMARY_FORK_ID=0
