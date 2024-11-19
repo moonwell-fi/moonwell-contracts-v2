3556ad6be3a215ec37e0fde8a88caef4d7cf36df
Ana Julia <anajuliabit@gmail.com>
2024-11-19 10:30:38 -0300

    add mip-x06

---
+++ proposals/mips/mip-x06/x06.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export MIP_REWARDS_PATH=src/proposals/mips/mip-x06/x06.json
+echo "MIP_REWARDS_PATH=$MIP_REWARDS_PATH"
+
+export DESCRIPTION_PATH=src/proposals/mips/mip-x06/x06.md
+echo "DESCRIPTION_PATH=$DESCRIPTION_PATH"
+
+export PRIMARY_FORK_ID=0
+echo "PRIMARY_FORK_ID=$PRIMARY_FORK_ID"
