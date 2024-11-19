4d2bb8c9665a665e5c87b1aec9ec532bc1521271
Ana Julia <anajuliabit@gmail.com>
2024-11-19 14:16:48 -0300

    example proposal mip x07

---
+++ proposals/mips/mip-x07/x07.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export JSON_PATH=proposals/mips/mip-x07/x07.json
+echo "JSON_PATH=$JSON_PATH"
+
+export DESCRIPTION_PATH=proposals/mips/mip-x07/x07.md
+echo "DESCRIPTION_PATH=$DESCRIPTION_PATH"
+
+export PRIMARY_FORK_ID=1
+echo "PRIMARY_FORK_ID=$PRIMARY_FORK_ID"
+
