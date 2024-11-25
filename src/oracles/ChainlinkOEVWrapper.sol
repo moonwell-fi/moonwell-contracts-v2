4accc28f4cf1e0528b80a91c02a69b10a492b62d
Ana Julia <anajuliabit@gmail.com>
2024-11-25 11:06:33 -0300

    add nastpec comments

--- src/oracles/ChainlinkOEVWrapper.sol
+++ src/oracles/ChainlinkOEVWrapper.sol
@@ -4,6 +4,9 @@ pragma solidity 0.8.19;
 import {Ownable} from "@openzeppelin-contracts/contracts/access/Ownable.sol";
 import {AggregatorV3Interface} from "./AggregatorV3Interface.sol";
 
+/// @title ChainlinkFeedOEVWrapper
+/// @notice A wrapper for Chainlink price feeds that allows early updates with a fee
+/// @dev This contract implements the AggregatorV3Interface and adds OEV (Oracle Extractable Value) functionality
 contract ChainlinkFeedOEVWrapper is AggregatorV3Interface, Ownable {
     event FeeMultiplierChanged(uint16 newFee);
     event EarlyUpdateWindowChanged(uint256 newWindow);
@@ -15,6 +18,11 @@ contract ChainlinkFeedOEVWrapper is AggregatorV3Interface, Ownable {
     uint256 private cachedTimestamp;
     int256 private cachedPrice;
 
+    /// @notice Constructor to initialize the wrapper
+    /// @param _originalFeed Address of the original Chainlink feed
+    /// @param _earlyUpdateWindow Time window for early updates
+    /// @param _feeMultiplier Multiplier for the fee calculation
+    /// @param _owner Address of the contract owner
     constructor(
         address _originalFeed,
         uint256 _earlyUpdateWindow,
@@ -36,6 +44,13 @@ contract ChainlinkFeedOEVWrapper is AggregatorV3Interface, Ownable {
         transferOwnership(_owner);
     }
 
+    /// @notice Get the latest round data
+    /// @dev Returns cached data if within early update window, otherwise fetches from original feed
+    /// @return roundId The round ID
+    /// @return answer The price
+    /// @return startedAt The timestamp when the round started
+    /// @return updatedAt The timestamp when the round was updated
+    /// @return answeredInRound The round ID in which the answer was computed
     function latestRoundData()
         external
         view
@@ -55,6 +70,8 @@ contract ChainlinkFeedOEVWrapper is AggregatorV3Interface, Ownable {
         }
     }
 
+    /// @notice Update the price earlier than the standard update interval
+    /// @dev Requires payment of a fee based on gas price and fee multiplier
     function updatePriceEarly() external payable {
         require(
             msg.value >= (tx.gasprice - block.basefee) * uint256(feeMultiplier),
@@ -62,7 +79,7 @@ contract ChainlinkFeedOEVWrapper is AggregatorV3Interface, Ownable {
         );
         require(
             block.timestamp > cachedTimestamp,
-            "New timestamp must be greather than current"
+            "New timestamp must be greater than current"
         );
 
         (, int256 price, , , ) = originalFeed.latestRoundData();
@@ -71,18 +88,31 @@ contract ChainlinkFeedOEVWrapper is AggregatorV3Interface, Ownable {
         cachedTimestamp = block.timestamp;
     }
 
+    /// @notice Get the number of decimals in the feed
+    /// @return The number of decimals
     function decimals() external view override returns (uint8) {
         return originalFeed.decimals();
     }
 
+    /// @notice Get the description of the feed
+    /// @return The description string
     function description() external view override returns (string memory) {
         return originalFeed.description();
     }
 
+    /// @notice Get the version number of the aggregator
+    /// @return The version number
     function version() external view override returns (uint256) {
         return originalFeed.version();
     }
 
+    /// @notice Get data about a specific round
+    /// @param _roundId The round ID to retrieve data for
+    /// @return roundId The round ID
+    /// @return answer The price
+    /// @return startedAt The timestamp when the round started
+    /// @return updatedAt The timestamp when the round was updated
+    /// @return answeredInRound The round ID in which the answer was computed
     function getRoundData(
         uint80 _roundId
     )
@@ -100,11 +130,17 @@ contract ChainlinkFeedOEVWrapper is AggregatorV3Interface, Ownable {
         return originalFeed.getRoundData(_roundId);
     }
 
+    /// @notice Set a new fee multiplier for early updates
+    /// @param newMultiplier The new fee multiplier to set
+    /// @dev Only callable by the contract owner
     function setFeeMultiplier(uint16 newMultiplier) public onlyOwner {
         feeMultiplier = newMultiplier;
         emit FeeMultiplierChanged(newMultiplier);
     }
 
+    /// @notice Set a new early update window
+    /// @param newWindow The new early update window duration in seconds
+    /// @dev Only callable by the contract owner
     function setEarlyUpdateWindow(uint256 newWindow) public onlyOwner {
         earlyUpdateWindow = newWindow;
         emit EarlyUpdateWindowChanged(newWindow);
