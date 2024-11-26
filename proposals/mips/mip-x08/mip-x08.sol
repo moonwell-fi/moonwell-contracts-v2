2fe042a7520f09477aafc968a3c307d1b1852b31
anajuliabit <anajuliabit@gmail.com>
2024-11-26 11:07:11 -0300

    fix approval

--- proposals/mips/mip-x08/mip-x08.sol
+++ proposals/mips/mip-x08/mip-x08.sol
@@ -29,10 +29,7 @@ contract mipx08 is HybridProposal {
         vm.selectFork(BASE_FORK_ID);
 
         vm.startPrank(addresses.getAddress("FOUNDATION_MULTISIG"));
-        IERC20(addresses.getAddress("WELL")).approve(
-            addresses.getAddress("xWELL_PROXY", BASE_CHAIN_ID),
-            WELL_AMOUNT
-        );
+        IERC20(addresses.getAddress("xWELL_PROXY")).approve(WELL_AMOUNT);
         vm.stopPrank();
     }
 
