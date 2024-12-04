e60235afa37062daab913440495c98e02aaf7690
Elliot <elliotfriedman3@gmail.com>
2024-12-03 22:28:34 -0800

    fix: bump to 18m WELL
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- proposals/mips/mip-x08/mip-x08.sol
+++ proposals/mips/mip-x08/mip-x08.sol
@@ -12,7 +12,7 @@ import {HybridProposal, ActionType} from "@proposals/proposalTypes/HybridProposa
 contract mipx08 is HybridProposal {
     string public constant override name = "MIP-X07";
 
-    uint256 public constant WELL_AMOUNT = 16_000_000e18;
+    uint256 public constant WELL_AMOUNT = 18_000_000e18;
     uint256 public urdBalanceBefore;
 
     constructor() {
