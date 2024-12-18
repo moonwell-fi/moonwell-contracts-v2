b3f3bd0492dbf33e9cd53b2052830f36c7567f3b
Elliot <elliotfriedman3@gmail.com>
2024-12-17 22:28:17 -0800

    add: mip-x09
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

---
+++ proposals/mips/mip-x09/x09.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export MIP_REWARDS_PATH=proposals/mips/mip-x09/x09.json
+echo "MIP_REWARDS_PATH=$JSON_PATH"
+
+export DESCRIPTION_PATH=proposals/mips/mip-x09/x09.md
+echo "DESCRIPTION_PATH=$DESCRIPTION_PATH"
+
+export PRIMARY_FORK_ID=0
+echo "PRIMARY_FORK_ID=$PRIMARY_FORK_ID"
