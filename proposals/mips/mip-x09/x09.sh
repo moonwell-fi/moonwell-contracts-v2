b1604b26c2a86ff40e7b141c01d1b1095abffc5d
Elliot <elliotfriedman3@gmail.com>
2024-12-18 08:18:50 -0800

    fix error in x09 shell script
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- proposals/mips/mip-x09/x09.sh
+++ proposals/mips/mip-x09/x09.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export MIP_REWARDS_PATH=proposals/mips/mip-x09/x09.json
-echo "MIP_REWARDS_PATH=$JSON_PATH"
+echo "MIP_REWARDS_PATH=$MIP_REWARDS_PATH"
 
 export DESCRIPTION_PATH=proposals/mips/mip-x09/x09.md
 echo "DESCRIPTION_PATH=$DESCRIPTION_PATH"
