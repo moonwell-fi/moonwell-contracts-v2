25df71307df0ac3ef203ff33afb709c115410e9b
Elliot <elliotfriedman3@gmail.com>
2025-01-09 00:16:36 -0800

    remove: reserve registry
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- src/market/ReserveRegistry.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity =0.8.19;
-
-contract ReserveRegistry {
-    /// @notice maps the mToken to the automation module address to receive assets
-    mapping(address => address) public mTokenToAutomationModule;
-}
