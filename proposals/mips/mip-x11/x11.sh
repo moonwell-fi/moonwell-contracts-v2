3927fd616d352a2b5427cf42ec4541344fd95acf
anajuliabit <anajuliabit@gmail.com>
2025-01-14 14:07:38 -0300

    add mip-x11

---
+++ proposals/mips/mip-x11/x11.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export MIP_REWARDS_PATH=proposals/mips/mip-x11/x11.json
+echo "MIP_REWARDS_PATH=$MIP_REWARDS_PATH"
+
+export DESCRIPTION_PATH=proposals/mips/mip-x11/x11.md
+echo "DESCRIPTION_PATH=$DESCRIPTION_PATH"
+
+export PRIMARY_FORK_ID=0
+echo "PRIMARY_FORK_ID=$PRIMARY_FORK_ID"
