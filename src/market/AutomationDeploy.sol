635955c7dfa5242fc390d5977fae493268faec61
Elliot <elliotfriedman3@gmail.com>
2025-01-17 18:46:00 -0800

    check mToken and reserve asset match
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- src/market/AutomationDeploy.sol
+++ src/market/AutomationDeploy.sol
@@ -1,5 +1,6 @@
 pragma solidity =0.8.19;
 
+import {MErc20Storage} from "@protocol/MTokenInterfaces.sol";
 import {ReserveAutomation} from "@protocol/market/ReserveAutomation.sol";
 import {ERC20HoldingDeposit} from "@protocol/market/ERC20HoldingDeposit.sol";
 
@@ -24,6 +25,11 @@ contract AutomationDeploy {
         require(
             params.mTokenMarket.code.length > 0,
             "mTokenMarket must be set"
+        );
+        require(
+            MErc20Storage(params.mTokenMarket).underlying() ==
+                params.reserveAsset,
+            "reserveUnderlying must match mToken underlying"
         );
         require(params.owner != address(0), "owner must be set");
         require(params.guardian != address(0), "guardian must be set");
