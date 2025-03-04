57ccf347e518d69c2fc6257daa68d1338fc7d00d
Elliot <elliotfriedman3@gmail.com>
2025-03-03 23:35:55 -0800

    add post deploy assertions
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- script/DeployMultiRewards.s.sol
+++ script/DeployMultiRewards.s.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.19;
 
 import {Script} from "@forge-std/Script.sol";
 import {console} from "@forge-std/console.sol";
+import {IMultiRewards} from "crv-rewards/IMultiRewards.sol";
 import {AllChainAddresses as Addresses} from "@proposals/Addresses.sol";
 
 /// test commands:
@@ -15,14 +16,22 @@ contract DeployMultiRewards is Script {
     function run() public {
         Addresses addresses = new Addresses();
 
+        address owner = addresses.getAddress("TEMPORAL_GOVERNOR");
+        address stakingToken = addresses.getAddress("USDC_METAMORPHO_VAULT");
+
         vm.startBroadcast();
 
         address multiRewards = deployCode(
             "artifacts/foundry/MultiRewards.sol/MultiRewards.json",
-            abi.encode(
-                addresses.getAddress("TEMPORAL_GOVERNOR"),
-                addresses.getAddress("USDC_METAMORPHO_VAULT")
-            )
+            abi.encode(owner, stakingToken)
+        );
+
+        // Verify owner and stakingToken are set correctly
+        IMultiRewards rewards = IMultiRewards(multiRewards);
+        require(rewards.owner() == owner, "Owner not set correctly");
+        require(
+            address(rewards.stakingToken()) == stakingToken,
+            "Staking token not set correctly"
         );
 
         vm.stopBroadcast();
