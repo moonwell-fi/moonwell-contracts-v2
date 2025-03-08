6a4445c0e70c65ffd35aec3353792932e2ecee18
Ana Bittencourt <anajuliabit@gmail.com>
2025-03-08 07:01:38 -0300

    Add mip-x16

--- proposals/mips/mip-x16/x16.sh
+++ proposals/mips/mip-x16/x16.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export MIP_REWARDS_PATH=proposals/mips/mip-x16/x16.json
+export MIP_REWARDS_PATH=proposals/mips/mip-x13/x13.json
 echo "MIP_REWARDS_PATH=$MIP_REWARDS_PATH"
 
-export DESCRIPTION_PATH=proposals/mips/mip-x16/x16.md
+export DESCRIPTION_PATH=proposals/mips/mip-x13/x13.md
 echo "DESCRIPTION_PATH=$DESCRIPTION_PATH"
 
 export PRIMARY_FORK_ID=0
