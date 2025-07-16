874bd03e72abe9b3393d541002d5be33a5f97e34
Chris <cnobleshriver@gmail.com>
2025-07-16 20:45:45 +0100

    init mip-x27

---
+++ proposals/mips/mip-x27/x27.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export JSON_PATH=proposals/mips/mip-x27/x27.json
+echo "JSON_PATH=$JSON_PATH"
+
+export DESCRIPTION_PATH=proposals/mips/mip-x27/x27.md
+echo "DESCRIPTION_PATH=$DESCRIPTION_PATH"
+
+export PRIMARY_FORK_ID=0
+echo "PRIMARY_FORK_ID=$PRIMARY_FORK_ID"
