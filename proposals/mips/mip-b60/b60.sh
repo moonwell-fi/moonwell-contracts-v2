1f12166df2b8bd98e683f29eaefcb4766c3f55fc
Chris <cnobleshriver@gmail.com>
2026-06-23 13:43:29 -0400

    init b60

---
+++ proposals/mips/mip-b60/b60.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export JSON_PATH=proposals/mips/mip-b60/b60.json
+export DESCRIPTION_PATH=proposals/mips/mip-b60/b60.md
+export PRIMARY_FORK_ID=1
+
+echo "JSON_PATH=$JSON_PATH"
+echo "DESCRIPTION_PATH=$DESCRIPTION_PATH"
+echo "PRIMARY_FORK_ID=$PRIMARY_FORK_ID"
