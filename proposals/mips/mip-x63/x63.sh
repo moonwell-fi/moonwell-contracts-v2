66636f3730e9a711081c0b57a37ff541b9c97b6e
Chris <cnobleshriver@gmail.com>
2026-07-20 06:43:51 +0530

    init mip-x63

---
+++ proposals/mips/mip-x63/x63.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+export JSON_PATH=proposals/mips/mip-x63/x63.json
+export DESCRIPTION_PATH=proposals/mips/mip-x63/x63.md
+export PRIMARY_FORK_ID=3
+
+echo "JSON_PATH=$JSON_PATH"
+echo "DESCRIPTION_PATH=$DESCRIPTION_PATH"
+echo "PRIMARY_FORK_ID=$PRIMARY_FORK_ID"
